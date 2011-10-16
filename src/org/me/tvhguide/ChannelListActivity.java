316ab35082069340befe459523969050edbb9e55
John Törnblom <john.tornblom@gmail.com>
2011-10-16 17:03:59 +0200

    cosmetics

--- src/org/me/tvhguide/ChannelListActivity.java
+++ src/org/me/tvhguide/ChannelListActivity.java
@@ -44,7 +44,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TableRow;
