d8761d2ea0517ae63303e8e04040237c3aa5a8bf
John Törnblom <john.tornblom@gmail.com>
2012-02-28 10:10:08 +0100

    cosmetics

--- src/org/me/tvhguide/ProgrammeActivity.java
+++ src/org/me/tvhguide/ProgrammeActivity.java
@@ -56,6 +56,7 @@ public class ProgrammeActivity extends Activity {
         for (Programme p : channel.epg) {
             if (p.id == eventId) {
                 programme = p;
+                break;
             }
         }
 
