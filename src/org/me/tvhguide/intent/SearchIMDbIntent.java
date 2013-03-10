4cfb37a2fbfb18aa3ef63571f2a37b96d10e7252
John Törnblom <john@workstation.(none)>
2013-03-10 15:18:28 +0100

    query imdb titles rather than any random category (only works with the web-based version of imdb)

--- src/org/me/tvhguide/intent/SearchIMDbIntent.java
+++ src/org/me/tvhguide/intent/SearchIMDbIntent.java
@@ -19,11 +19,11 @@ public class SearchIMDbIntent extends Intent {
     public SearchIMDbIntent(Context ctx, String query) {
         super(Intent.ACTION_VIEW);
 
-        setData(Uri.parse("imdb:///find?q=" + URLEncoder.encode(query)));
+        setData(Uri.parse("imdb:///find?s=tt&q=" + URLEncoder.encode(query)));
 
         PackageManager packageManager = ctx.getPackageManager();
         if (packageManager.queryIntentActivities(this, PackageManager.MATCH_DEFAULT_ONLY).isEmpty()) {
-            setData(Uri.parse("http://akas.imdb.org/find?q=" + URLEncoder.encode(query)));
+            setData(Uri.parse("http://akas.imdb.org/find?s=tt&q=" + URLEncoder.encode(query)));
         }
     }
 }
