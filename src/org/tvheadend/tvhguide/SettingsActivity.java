a2ca67c63780e23fd590ee01851f1c23c35b0e18
Robert Siebert <rsiebert@miroku.no-ip.com>
2013-09-30 15:19:40 +0200

    Changed the title of the settings activity.

--- src/org/tvheadend/tvhguide/SettingsActivity.java
+++ src/org/tvheadend/tvhguide/SettingsActivity.java
@@ -1,13 +1,22 @@
 package org.tvheadend.tvhguide;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 public class SettingsActivity extends PreferenceActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Apply the specified theme
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Boolean theme = prefs.getBoolean("lightThemePref", false);
+        setTheme(theme ? android.R.style.Theme_Holo_Light : android.R.style.Theme_Holo);
+        
+        setTitle(R.string.menu_settings);
         
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
