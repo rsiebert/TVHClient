e375203b7eda3b5dbe90b313cba8f1430e53b639
Robert Siebert <rsiebert@miroku.no-ip.com>
2013-10-02 10:40:56 +0200

    Removed unused import.

--- src/org/tvheadend/tvhguide/RecordingListFragment.java
+++ src/org/tvheadend/tvhguide/RecordingListFragment.java
@@ -30,7 +30,6 @@ import org.tvheadend.tvhguide.model.Recording;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
