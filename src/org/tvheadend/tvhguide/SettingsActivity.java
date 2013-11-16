66bfa6e1aed07a84a7adf533fae30dd70b68a153
Robert Siebert <rsiebert@miroku.no-ip.com>
2013-11-16 13:59:01 +0100

    Add the actionbar and allow pressing the home button.

--- src/org/tvheadend/tvhguide/SettingsActivity.java
+++ src/org/tvheadend/tvhguide/SettingsActivity.java
@@ -26,23 +26,42 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceManager;
 import android.support.v4.preference.PreferenceFragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class SettingsActivity extends ActionBarActivity {
 
+    private ActionBar actionBar = null;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
-        // Apply the specified theme
         setTheme(Utils.getThemeId(this));
         super.onCreate(savedInstanceState);
-        setTitle(R.string.menu_settings);
+
+        // Setup the action bar and show the title
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setTitle(R.string.menu_settings);
 
         // Show the specified fragment
         getSupportFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
     }
-    
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            onBackPressed();
+            return true;
+            
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
     public static class SettingsFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
         
         @Override
