ac221fe1ac9eca4854a7afc6aa01a375e5bd4aa1
Robert Siebert <rsiebert@miroku.no-ip.com>
2013-11-16 11:01:57 +0100

    Use the fragment class from the support library.

--- src/org/tvheadend/tvhguide/StatusFragment.java
+++ src/org/tvheadend/tvhguide/StatusFragment.java
@@ -24,9 +24,9 @@ import org.tvheadend.tvhguide.htsp.HTSListener;
 import org.tvheadend.tvhguide.htsp.HTSService;
 import org.tvheadend.tvhguide.model.Recording;
 
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
