0361e9ee691f0ac087e2737ad7ad55a97575342b
Robert Siebert <rsiebert@miroku.no-ip.com>
2014-02-27 13:13:59 +0100

    Fixed some lint warnings.

--- src/org/tvheadend/tvhguide/model/SeriesInfo.java
+++ src/org/tvheadend/tvhguide/model/SeriesInfo.java
@@ -1,5 +1,7 @@
 package org.tvheadend.tvhguide.model;
 
+import java.util.Locale;
+
 public class SeriesInfo {
 
 	public int seasonNumber;
@@ -33,7 +35,7 @@ public class SeriesInfo {
 		}
 
 		if(s.length() > 0) {
-			s = s.substring(0,1).toUpperCase() + s.substring(1);
+			s = s.substring(0,1).toLowerCase(Locale.getDefault()) + s.substring(1);
 		}
 		
 		return s;
