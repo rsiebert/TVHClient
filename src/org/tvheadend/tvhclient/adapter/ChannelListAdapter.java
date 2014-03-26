bae1b294d98b78834170f1081f6edfaf96eb7187
Robert Siebert <rsiebert@miroku.no-ip.com>
2014-03-26 13:48:27 +0100

    Pass the icon text (if required) to the method. If icons shall be shown but there is no icon available, display the channel name instead.

--- src/org/tvheadend/tvhclient/adapter/ChannelListAdapter.java
+++ src/org/tvheadend/tvhclient/adapter/ChannelListAdapter.java
@@ -61,6 +61,7 @@ public class ChannelListAdapter extends ArrayAdapter<Channel> {
 
     static class ViewHolder {
         public ImageView icon;
+        public TextView icon_text;
         public TextView title;
         public TextView channel;
         public TextView time;
@@ -79,6 +80,7 @@ public class ChannelListAdapter extends ArrayAdapter<Channel> {
             view = context.getLayoutInflater().inflate(layout, null);
             holder = new ViewHolder();
             holder.icon = (ImageView) view.findViewById(R.id.icon);
+            holder.icon_text = (TextView) view.findViewById(R.id.icon_text);
             holder.title = (TextView) view.findViewById(R.id.title);
             holder.channel = (TextView) view.findViewById(R.id.channel);
             holder.progress = (ProgressBar) view.findViewById(R.id.progress);
@@ -103,7 +105,7 @@ public class ChannelListAdapter extends ArrayAdapter<Channel> {
                 holder.channel.setText(c.name);
             }
             if (holder.icon != null) {
-                Utils.setChannelIcon(holder.icon, null, c);
+                Utils.setChannelIcon(holder.icon, holder.icon_text, null, c);
 
                 // Add the listener to the icon so that a 
                 // click calls the program list of this channel
