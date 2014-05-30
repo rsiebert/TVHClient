91e297c79c4e1d232cc4ece96c11db5cb208b693
Robert Siebert <rsiebert@miroku.no-ip.com>
2014-05-30 12:07:08 +0200

    Made a constant variable final.

--- src/org/tvheadend/tvhclient/Constants.java
+++ src/org/tvheadend/tvhclient/Constants.java
@@ -8,5 +8,5 @@ public class Constants {
     public static final int RESULT_CODE_START_PLAYER = 104;
     
     // Amount of programs of a channel that shall be loaded from the server 
-    public static int PREF_PROGRAMS_TO_LOAD = 15;
+    public static final int PREF_PROGRAMS_TO_LOAD = 15;
 }
