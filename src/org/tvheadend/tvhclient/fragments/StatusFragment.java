4c67e32d87073a535bea769c393926bdaf760841
Robert Siebert <rsiebert@miroku.no-ip.com>
2014-08-29 11:52:34 +0200

    Removed logging.

--- src/org/tvheadend/tvhclient/fragments/StatusFragment.java
+++ src/org/tvheadend/tvhclient/fragments/StatusFragment.java
@@ -34,7 +34,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -75,7 +74,6 @@ public class StatusFragment extends Fragment implements HTSListener {
         Bundle bundle = getArguments();
         if (bundle != null) {
             connectionStatus  = bundle.getString(Constants.BUNDLE_CONNECTION_STATUS);
-            Log.d(TAG, "onCreateView " + connectionStatus);
         }
 
         View v = inflater.inflate(R.layout.status_fragment_layout, container, false);
