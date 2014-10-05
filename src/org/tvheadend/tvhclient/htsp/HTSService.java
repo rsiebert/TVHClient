4b488396fa4118ee31aaee67a391dfbfcfae6588
Robert Siebert <rsiebert@miroku.no-ip.com>
2014-10-05 09:37:35 +0200

    Fix the identifier string for an episode. According to the htsp website it is "episodeOnscreen" instead of "onScreen".

--- src/org/tvheadend/tvhclient/htsp/HTSService.java
+++ src/org/tvheadend/tvhclient/htsp/HTSService.java
@@ -717,7 +717,7 @@ public class HTSService extends Service implements HTSConnectionListener {
         SeriesInfo info = new SeriesInfo();
         info.episodeCount = msg.getInt("episodeCount", 0);
         info.episodeNumber = msg.getInt("episodeNumber", 0);
-        info.onScreen = msg.getString("onScreen", "");
+        info.onScreen = msg.getString("episodeOnscreen", "");
         info.partCount = msg.getInt("partCount", 0);
         info.partNumber = msg.getInt("partNumber", 0);
         info.seasonCount = msg.getInt("seasonCount", 0);
