f917a6b6142108c9de7f4b22e7f77559178b2049
Robert Siebert <rsiebert@miroku.no-ip.com>
2015-01-04 22:34:37 +0100

    Remove unused code

--- src/org/tvheadend/tvhclient/model/DrawerMenuItem.java
+++ src/org/tvheadend/tvhclient/model/DrawerMenuItem.java
@@ -6,41 +6,9 @@ public class DrawerMenuItem {
     // Default variables
     public int id = -1;
     public int icon = 0;
-    public String header = "";
     public String title = "";
     public int count = 0;
-    public boolean isMenu = false;
-    public boolean isEnabled = true;
     public boolean isVisible = true;
-
-    /**
-     * Creates an header item with no text
-     */
-    public DrawerMenuItem() {
-        // NOP
-    }
-
-    /**
-     * Creates an header item with the given text
-     * 
-     * @param header
-     */
-    public DrawerMenuItem(String header) {
-        this.header = header;
-    }
-
-    /**
-     * Creates a menu item with the given title and id. The id is used to
-     * identify which menu was selected
-     * 
-     * @param id
-     * @param title
-     */
-    public DrawerMenuItem(int id, String title) {
-        this.id = id;
-        this.title = title;
-        this.isMenu = true;
-    }
 
     /**
      * Creates a menu item with the given title and id. The id is used to
@@ -54,6 +22,5 @@ public class DrawerMenuItem {
         this.id = id;
         this.icon = icon;
         this.title = title;
-        this.isMenu = true;
     }
 }
