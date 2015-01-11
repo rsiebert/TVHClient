b20bd848bd31641fef4814323e641b019bc9529b
Robert Siebert <rsiebert@miroku.no-ip.com>
2015-01-11 22:19:02 +0100

    Removed logging.

--- src/org/tvheadend/tvhclient/adapter/TimerRecordingListAdapter.java
+++ src/org/tvheadend/tvhclient/adapter/TimerRecordingListAdapter.java
@@ -29,7 +29,6 @@ import org.tvheadend.tvhclient.model.TimerRecording;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
