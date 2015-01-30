5e038ebb67f47353a86d05f773364ad2ccea89d9
Robert Siebert <rsiebert@miroku.no-ip.com>
2015-01-30 20:18:17 +0100

    Set default values when creating a profile.

--- src/org/tvheadend/tvhclient/model/Profile.java
+++ src/org/tvheadend/tvhclient/model/Profile.java
@@ -1,17 +1,17 @@
 package org.tvheadend.tvhclient.model;
 
 public class Profile {
-    public int id;
+    public int id = 0;
     // use the new profile names (requires htsp api version > 15)
-    public boolean enabled;
+    public boolean enabled = false;
     // The uuid that the defines the profile to be used
-    public String uuid;
+    public String uuid = "";
 
     // The old profile definitions that are passed to the server in the url 
-    public String container;
-    public boolean transcode;
-    public String resolution;
-    public String audio_codec;
-    public String video_codec;
-    public String subtitle_codec;
+    public String container = "matroska";
+    public boolean transcode = false;
+    public String resolution = "384";
+    public String audio_codec = "AAC";
+    public String video_codec = "H264";
+    public String subtitle_codec = "NONE";
 }
