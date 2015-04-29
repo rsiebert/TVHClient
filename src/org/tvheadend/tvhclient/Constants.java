e8d256bbddda821cb84a0fa9acb362b4c76ed9ad
Robert Siebert <rsiebert@miroku.no-ip.com>
2015-04-29 15:30:10 +0200

    Added constant that identifies the in-app item.

--- src/org/tvheadend/tvhclient/Constants.java
+++ src/org/tvheadend/tvhclient/Constants.java
@@ -2,6 +2,9 @@ package org.tvheadend.tvhclient;
 
 public class Constants {
 
+    // Product id for the in-app billing item to unlock the application
+    public static final String UNLOCKER = "unlocker";
+
     // Codes to identify the returning activity
     public static final int RESULT_CODE_SETTINGS = 101;
     public static final int RESULT_CODE_START_PLAYER = 102;
