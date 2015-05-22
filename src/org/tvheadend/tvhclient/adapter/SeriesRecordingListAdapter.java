d84ac48a187b03fe03470a03356e2d9fe24c0d85
Robert Siebert <rsiebert@miroku.no-ip.com>
2015-05-22 22:47:56 +0200

    Check against null before comparing.

--- src/org/tvheadend/tvhclient/adapter/SeriesRecordingListAdapter.java
+++ src/org/tvheadend/tvhclient/adapter/SeriesRecordingListAdapter.java
@@ -36,14 +36,20 @@ public class SeriesRecordingListAdapter extends ArrayAdapter<SeriesRecording> {
         case Constants.RECORDING_SORT_ASCENDING:
             sort(new Comparator<SeriesRecording>() {
                 public int compare(SeriesRecording x, SeriesRecording y) {
-                    return (y.title.compareTo(x.title));
+                    if (x != null && y != null && x.title != null && y.title != null) {
+                        return (y.title.compareTo(x.title));
+                    }
+                    return 0;
                 }
             });
         break;
         case Constants.RECORDING_SORT_DESCENDING:
             sort(new Comparator<SeriesRecording>() {
                 public int compare(SeriesRecording x, SeriesRecording y) {
-                    return (x.title.compareTo(y.title));
+                    if (x != null && y != null && x.title != null && y.title != null) {
+                        return (x.title.compareTo(y.title));
+                    }
+                    return 0;
                 }
             });
             break;
