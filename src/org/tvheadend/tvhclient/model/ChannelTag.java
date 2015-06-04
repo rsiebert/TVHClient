b36cf91a0a73c38dfc41749c830551d86ce29a03
TMHBOFH <itsystem.bofh@gmail.com>
2015-06-04 23:10:52 +0200

    Update ChannelTag.java

--- src/org/tvheadend/tvhclient/model/ChannelTag.java
+++ src/org/tvheadend/tvhclient/model/ChannelTag.java
@@ -1,7 +1,5 @@
 package org.tvheadend.tvhclient.model;
 
-import java.util.Comparator;
-import java.util.Locale;
 import android.graphics.Bitmap;
 
 public class ChannelTag {
@@ -10,16 +8,6 @@ public class ChannelTag {
     public String name;
     public String icon;
     public Bitmap iconBitmap;
-
-    public static Comparator<ChannelTag> ChannelTagNameSorter = new Comparator<ChannelTag>() {
-        public int compare(ChannelTag x, ChannelTag y) {
-            if (x != null && y != null && x.name != null && y.name != null) {
-                return x.name.toLowerCase(Locale.getDefault()).compareTo(
-                        y.name.toLowerCase(Locale.getDefault()));
-            }
-            return 0;
-        }
-    };
 
     @Override
     public String toString() {
