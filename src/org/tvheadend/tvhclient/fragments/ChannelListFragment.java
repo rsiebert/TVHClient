24972eb8a234a8b92a67026674e8367455860e57
Robert Siebert <rsiebert@miroku.no-ip.com>
2016-02-21 21:17:59 +0100

    Removed unused import

--- src/org/tvheadend/tvhclient/fragments/ChannelListFragment.java
+++ src/org/tvheadend/tvhclient/fragments/ChannelListFragment.java
@@ -36,7 +36,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
