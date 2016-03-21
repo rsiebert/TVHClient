3a2571e00d6ac16c6df859b8734780bd6efe7984
Robert Siebert <rsiebert@miroku.no-ip.com>
2016-03-21 19:53:51 +0100

    Only disable the external storage download if the setting is active

--- src/org/tvheadend/tvhclient/MainActivity.java
+++ src/org/tvheadend/tvhclient/MainActivity.java
@@ -421,9 +421,11 @@ public class MainActivity extends ActionBarActivity implements SearchView.OnQuer
         };
 
         // Check if external download is possible, otherwise disable the settings
-        if (!app.isUnlocked() && !Utils.isExternalStorageWritable()) {
-            app.log(TAG, "External storage not available, disabling option");
-            prefs.edit().putBoolean("pref_download_to_external_storage", false).apply();
+        if (prefs.getBoolean("pref_download_to_external_storage", false)) {
+            if (!app.isUnlocked() || !Utils.isExternalStorageWritable()) {
+                app.log(TAG, "External storage not available, disabling option");
+                prefs.edit().putBoolean("pref_download_to_external_storage", false).apply();
+            }
         }
     }
 
