3d9d2a5d3400324fa6ebc4d1f6907dafb2a8a001
Robert Siebert <rsiebert@miroku.no-ip.com>
2016-07-10 21:51:51 +0200

    Revert "Added new billing library functionality"
    This reverts commit 4a2531c84a64966c9368cebe30a4762dcf233a13.
    
    # Conflicts:
    #	app/src/main/java/org/tvheadend/tvhclient/TVHClientApplication.java

--- app/src/main/java/org/tvheadend/tvhclient/UnlockerActivity.java
+++ app/src/main/java/org/tvheadend/tvhclient/UnlockerActivity.java
@@ -1,5 +1,11 @@
 package org.tvheadend.tvhclient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Locale;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -12,21 +18,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
 
-import org.tvheadend.tvhclient.interfaces.HTSListener;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Locale;
-
 @SuppressWarnings("deprecation")
-public class UnlockerActivity extends ActionBarActivity implements HTSListener {
+public class UnlockerActivity extends ActionBarActivity {
 
     private final static String TAG = UnlockerActivity.class.getSimpleName();
 
     private CoordinatorLayout coordinatorLayout;
-    private TVHClientApplication app;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,8 +32,7 @@ public class UnlockerActivity extends ActionBarActivity implements HTSListener {
         setContentView(R.layout.webview_layout);
         Utils.setLanguage(this);
 
-        app = (TVHClientApplication) getApplication();
-        app.addListener(this);
+        TVHClientApplication app = (TVHClientApplication) getApplication();
 
         // Setup the action bar and show the title
         ActionBar actionBar = getSupportActionBar();
@@ -110,12 +106,6 @@ public class UnlockerActivity extends ActionBarActivity implements HTSListener {
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        app.removeListener(this);
-    }
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.purchase_cancel_menu, menu);
@@ -133,20 +123,21 @@ public class UnlockerActivity extends ActionBarActivity implements HTSListener {
         case R.id.menu_purchase:
             app.log(TAG, "Unlocker purchase button pressed");
 
-            // TODO
-            // for security, generate your payload here for verification.
-            // See the comments on verifyDeveloperPayload() for more info.
-            String payload = "";
-
             // Open the activity where the user can actually make the purchase
-            if (app.isBillingSetupDone()) {
+            if (app.getBillingProcessor().isInitialized()) {
                 app.log(TAG, "Billing initialized, showing unlocker dialog");
-                app.getOpenIabHelper().launchPurchaseFlow(this, Constants.UNLOCKER, 10001, app, payload);
-            } else {
-                app.log(TAG, "Billing not initialized, not showing unlocker dialog");
-                Snackbar.make(coordinatorLayout, getString(R.string.unlocker_not_initialized), Snackbar.LENGTH_LONG).show();
+                app.getBillingProcessor().purchase(this, Constants.UNLOCKER);
             }
 
+            // Check if the user has already made the purchase. We check this
+            // here because this activity is not information about any changes
+            // via the billing event interface. 
+            if (app.getBillingProcessor().isPurchased(Constants.UNLOCKER)) {
+                app.log(TAG, "Unlocker already purchased");
+                Snackbar.make(coordinatorLayout, getString(R.string.unlocker_already_purchased), 
+                        Snackbar.LENGTH_SHORT).show();
+                finish();
+            }
             return true;
 
         default:
@@ -156,10 +147,8 @@ public class UnlockerActivity extends ActionBarActivity implements HTSListener {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        app.log(TAG, "onActivityResult requestCode " + requestCode + ", resultCode " + resultCode);
         TVHClientApplication app = (TVHClientApplication) getApplication();
-        // Pass on the activity result to the helper for handling
-        if (!app.getOpenIabHelper().handleActivityResult(requestCode, resultCode, data)) {
+        if (!app.getBillingProcessor().handleActivityResult(requestCode, resultCode, data)) {
             app.log(TAG, "Unlocker purchase dialog was not shown or closed without action");
             // The billing activity was not shown or did nothing. Nothing needs
             // to be done
@@ -168,19 +157,5 @@ public class UnlockerActivity extends ActionBarActivity implements HTSListener {
         // Close this activity
         app.log(TAG, "Exiting unlocker"); 
         finish();
-    }
-
-    @Override
-    public void onMessage(String action, final Object obj) {
-        switch (action) {
-            case Constants.ACTION_SHOW_MESSAGE:
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        String msg = (String) obj;
-                        Snackbar.make(coordinatorLayout, msg, Snackbar.LENGTH_SHORT).show();
-                    }
-                });
-                break;
-        }
     }
 }
