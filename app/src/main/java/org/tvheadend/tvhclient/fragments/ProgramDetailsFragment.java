911818106aed7bf06377a65fe17c66ab60f6247b
Robert Siebert <robert.siebert@thalesgroup.com>
2016-08-30 15:08:58 +0200

    Removed unused import

--- app/src/main/java/org/tvheadend/tvhclient/fragments/ProgramDetailsFragment.java
+++ app/src/main/java/org/tvheadend/tvhclient/fragments/ProgramDetailsFragment.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.support.v7.widget.AppCompatButton;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
