1cf1765dd02c038f537c9f9ae78bb67be9d9ef72
Robert SIEBERT <Robert.SIEBERT@thalesgroup.com>
2016-09-20 08:55:18 +0200

    Added dialog to choose the download directory.

---
+++ app/src/main/java/org/tvheadend/tvhclient/adapter/FileBrowserListAdapter.java
@@ -0,0 +1,151 @@
+package org.tvheadend.tvhclient.adapter;
+
+import android.app.Activity;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.tvheadend.tvhclient.R;
+import org.tvheadend.tvhclient.Utils;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.List;
+
+public class FileBrowserListAdapter extends RecyclerView.Adapter<FileBrowserListAdapter.ViewHolder> {
+    private final Activity activity;
+    private File basePath;
+    private Callback mCallback;
+    private List<File> list;
+
+    public FileBrowserListAdapter(Activity activity, File basePath) {
+        this.activity = activity;
+        this.basePath = basePath;
+    }
+
+    public interface Callback {
+        void onItemClicked(int adapterPosition);
+        void onItemLongClicked(int adapterPosition);
+    }
+
+    public void setFileList(List<File> list, File basePath) {
+        this.list = list;
+        this.basePath = basePath;
+    }
+
+    public void setCallback(Callback mCallback) {
+        this.mCallback = mCallback;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.filebrowser_list_item, parent, false);
+        return new ViewHolder(view, this);
+    }
+
+    @Override
+    /**
+     * Applies the values to the available layout items
+     */
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final boolean lightTheme = (Utils.getThemeId(activity) == R.style.CustomTheme_Light);
+        final File item = list.get(position);
+        if (item != null) {
+            if (holder.icon != null) {
+                if (item.isFile()) {
+                    holder.icon.setImageResource(lightTheme ? R.drawable.ic_file_light : R.drawable.ic_file_dark);
+                } else if (item.isDirectory()) {
+                    holder.icon.setImageResource(lightTheme ? R.drawable.ic_folder_light : R.drawable.ic_folder_dark);
+                }
+            }
+
+            if (holder.title != null) {
+                // Show the option to navigate up if the first item is the base path.
+                if (position == 0 && item.getAbsolutePath().equals(basePath.getAbsolutePath())) {
+                    holder.title.setText("..");
+                } else {
+                    holder.title.setText(item.getName());
+                }
+                holder.title.setTag(position);
+            }
+
+            if (holder.itemCount != null) {
+                // Do not show the number of items for the parent directory
+                if (position == 0 && item.getAbsolutePath().equals(basePath.getAbsolutePath())) {
+                    holder.itemCount.setVisibility(View.GONE);
+                } else {
+
+                    FilenameFilter filter = new FilenameFilter() {
+                        @Override
+                        public boolean accept(File dir, String filename) {
+                            File f = new File(dir, filename);
+                            if (f.isDirectory() && !f.isHidden()) {
+                                return true;
+                            }
+                            return false;
+                        }
+                    };
+
+                    File[] files = item.listFiles(filter);
+                    final int count = (files != null) ? files.length : 0;
+                    holder.itemCount.setText(activity.getResources().getQuantityString(R.plurals.items, count, count));
+                    holder.itemCount.setVisibility(View.VISIBLE);
+                }
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    /**
+     * Provide a reference to the views for each data item
+     * Complex data items may need more than one view per item, and
+     * you provide access to all the views for a data item in a view holder
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+        final ImageView icon;
+        final TextView title;
+        final TextView itemCount;
+        final FileBrowserListAdapter adapter;
+
+        public ViewHolder(View view, FileBrowserListAdapter adapter) {
+            super(view);
+            this.icon = (ImageView) view.findViewById(R.id.icon);
+            this.title = (TextView) view.findViewById(R.id.title);
+            this.itemCount = (TextView) view.findViewById(R.id.item_count);
+
+            this.adapter = adapter;
+            view.setClickable(true);
+            view.setOnClickListener(this);
+            view.setOnLongClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (adapter.mCallback == null) {
+                return;
+            }
+            adapter.mCallback.onItemClicked(getAdapterPosition());
+        }
+
+        @Override
+        public boolean onLongClick(View view) {
+            if (adapter.mCallback == null) {
+                return false;
+            }
+            adapter.mCallback.onItemLongClicked(getAdapterPosition());
+            return true;
+        }
+    }
+}
---
+++ app/src/main/java/org/tvheadend/tvhclient/fragments/FileBrowserFragment.java
@@ -0,0 +1,241 @@
+package org.tvheadend.tvhclient.fragments;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Environment;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.TextView;
+
+import org.tvheadend.tvhclient.R;
+import org.tvheadend.tvhclient.Utils;
+import org.tvheadend.tvhclient.adapter.FileBrowserListAdapter;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+@SuppressWarnings("deprecation")
+public class FileBrowserFragment extends DialogFragment {
+
+    @SuppressWarnings("unused")
+    private final static String TAG = FileBrowserFragment.class.getSimpleName();
+
+    // TODO use a model instead of a list of files
+
+    private Activity activity;
+    private TextView currentPathView;
+    private Toolbar toolbar;
+    private View toolbarShadow;
+    private List<File> fileList = new ArrayList<>();
+
+    private FileBrowserListAdapter fileListAdapter;
+    private RecyclerView fileListView;
+    private RecyclerView.LayoutManager layoutManager;
+    private File basePath = Environment.getExternalStorageDirectory();
+    private File selectedPath;
+
+    public static FileBrowserFragment newInstance(Bundle args) {
+        FileBrowserFragment f = new FileBrowserFragment();
+        f.setArguments(args);
+        return f;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        // TODO min height and width
+
+        return dialog;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getDialog() != null) {
+            getDialog().getWindow().getAttributes().windowAnimations = R.style.dialog_animation_fade;
+            setStyle(DialogFragment.STYLE_NO_TITLE, Utils.getThemeId(activity));
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.activity = activity;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            // NOP
+        }
+
+        // Initialize all the widgets from the layout
+        View v = inflater.inflate(R.layout.filebrowser_layout, container, false);
+        currentPathView = (TextView) v.findViewById(R.id.path);
+        fileListView = (RecyclerView) v.findViewById(R.id.file_list);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        fileListView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(activity);
+        fileListView.setLayoutManager(layoutManager);
+
+        toolbar = (Toolbar) v.findViewById(R.id.toolbar);
+        toolbarShadow = v.findViewById(R.id.toolbar_shadow);
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        toolbar.setVisibility(getDialog() != null ? View.VISIBLE : View.GONE);
+        toolbarShadow.setVisibility(getDialog() != null ? View.VISIBLE : View.GONE);
+
+        if (getDialog() != null) {
+            toolbar.setTitle(R.string.select_directory);
+        }
+
+        if (currentPathView != null) {
+            currentPathView.setText(basePath.getAbsolutePath());
+        }
+
+        // Setup the menu in the toolbar and the listener to save the selected path
+        toolbar.inflateMenu(R.menu.filebrowser_menu);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.menu_select) {
+                    saveSelectedDirectory(selectedPath);
+                }
+                if (getDialog() != null) {
+                    getDialog().dismiss();
+                }
+                return true;
+            }
+        });
+
+        // Create the adapter that will show the contents of the selected path
+        fileListAdapter = new FileBrowserListAdapter(activity, basePath);
+        fileListView.setAdapter(fileListAdapter);
+
+        // Fill the adapter with the found files and directories
+        createFileList(basePath);
+        fileListAdapter.setFileList(getFileList(), basePath);
+        fileListAdapter.notifyDataSetChanged();
+
+        // Setup the listeners so that the user can navigate through the
+        // directory structure and also choose a directory by long pressing it
+        fileListAdapter.setCallback(new FileBrowserListAdapter.Callback() {
+            @Override
+            public void onItemClicked(int which) {
+                selectedPath = fileList.get(which);
+
+                // Get the contents of the newly selected path and update the adapter
+                createFileList(selectedPath);
+                fileListAdapter.setFileList(getFileList(), selectedPath.getParentFile());
+                fileListAdapter.notifyDataSetChanged();
+                if (currentPathView != null) {
+                    currentPathView.setText(selectedPath.getAbsolutePath());
+                }
+            }
+
+            @Override
+            public void onItemLongClicked(int which) {
+                // Save the selected folder in the preferences
+                selectedPath = fileList.get(which);
+                saveSelectedDirectory(selectedPath);
+            }
+        });
+    }
+
+    /**
+     *
+     * @param path
+     */
+    private void saveSelectedDirectory(File path) {
+        // TODO remove the path setDestinationInExternalPublicDir from the path?
+        Log.d(TAG, "Saving the selected path " + path.getAbsolutePath());
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        prefs.edit().putString("pref_download_directory", path.getAbsolutePath()).apply();
+    }
+
+    /**
+     * Returns the list of file objects that were found in the given directory
+     *
+     * @return fileList List of found files
+     */
+    private List<File> getFileList() {
+        return this.fileList;
+    }
+
+    /**
+     * Creates a list of file objects that are available in the given directory.
+     * The found files are only added to the list if they match the filter.
+     *
+     * @param path The directory that shall be searched
+     */
+    private void createFileList(File path) {
+        this.fileList.clear();
+
+        if (path.exists()) {
+
+            // Create the filter that checks if the file or directory shall be added to the list
+            FilenameFilter filter = new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String filename) {
+                    File f = new File(dir, filename);
+                    if (f.isDirectory() && !f.isHidden()) {
+                        return true;
+                    }
+                    return false;
+                }
+            };
+
+            // Save the contents from the file array into a list
+            File[] files = path.listFiles(filter);
+            if (files != null) {
+                for (File file : files) {
+                    fileList.add(file);
+                }
+            }
+
+            // Sort the found files alphabetically
+            Collections.sort(fileList, new Comparator<File>() {
+                @Override
+                public int compare(File file1, File file2) {
+                    return file1.getName().compareTo(file2.getName());
+                }
+            });
+
+            // Add the parent directory so the user can navigate backwards.
+            final File parent = path.getParentFile();
+            if (parent != null && !path.getPath().equals(basePath.getAbsolutePath())) {
+                fileList.add(0, parent);
+            }
+        }
+    }
+
+}
