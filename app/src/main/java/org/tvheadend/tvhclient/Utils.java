0e2a0cbc4faaa0257d05002c393f961ef1b187f4
Robert SIEBERT <Robert.SIEBERT@thalesgroup.com>
2016-09-22 17:50:36 +0200

    Set a default genre color in the switch statement.

--- app/src/main/java/org/tvheadend/tvhclient/Utils.java
+++ app/src/main/java/org/tvheadend/tvhclient/Utils.java
@@ -912,6 +912,9 @@ public class Utils {
             case 10:
                 color = R.color.EPG_SPECIAL;
                 break;
+            default:
+                color = android.R.color.transparent;
+                break;
             }
             SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
             
