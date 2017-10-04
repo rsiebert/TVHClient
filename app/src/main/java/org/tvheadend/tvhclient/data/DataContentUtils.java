9e7c2afa3f2c20addea46534a76662cd83d2486c
Robert Siebert <rsiebert@miroku.no-ip.com>
2017-10-04 23:57:01 +0200

    Add method to get the server information

--- app/src/main/java/org/tvheadend/tvhclient/data/DataContentUtils.java
+++ app/src/main/java/org/tvheadend/tvhclient/data/DataContentUtils.java
@@ -1,10 +1,12 @@
 package org.tvheadend.tvhclient.data;
 
+import android.content.ContentUris;
 import android.content.Context;
 import android.database.Cursor;
 import android.util.Log;
 
 import org.tvheadend.tvhclient.model.Connection;
+import org.tvheadend.tvhclient.model.ServerInfo;
 
 public class DataContentUtils {
     private static final String TAG = DataContentUtils.class.getName();
@@ -33,5 +35,26 @@ public class DataContentUtils {
             c.close();
         }
         return connection;
+    }
+
+    public static ServerInfo getActiveServerInfo(Context context) {
+        Log.d(TAG, "getActiveServerInfo() called with: context = [" + context + "]");
+
+        Cursor c = context.getApplicationContext().getContentResolver().query(
+                ContentUris.withAppendedId(DataContract.ServerInfo.CONTENT_URI_ACTIVE,
+                        DataContentProvider.SERVER_STATS_ID_ACTIVE),
+                null, null, null, null);
+
+        ServerInfo serverInfo = null;
+        if (c != null && c.getCount() > 0) {
+            serverInfo = new ServerInfo();
+            serverInfo.id = c.getLong(c.getColumnIndex(DataContract.ServerInfo.ID));
+            serverInfo.freeDiskSpace = c.getInt(c.getColumnIndex(DataContract.ServerInfo.FREE_DISC_SPACE));
+            serverInfo.totalDiskSpace = c.getInt(c.getColumnIndex(DataContract.ServerInfo.TOTAL_DISC_SPACE));
+            serverInfo.serverName = c.getString(c.getColumnIndex(DataContract.ServerInfo.SERVER_NAME));
+            serverInfo.serverVersion = c.getString(c.getColumnIndex(DataContract.ServerInfo.SERVER_VERSION));
+            c.close();
+        }
+        return serverInfo;
     }
 }
---
+++ app/src/main/java/org/tvheadend/tvhclient/model/ServerInfo.java
@@ -0,0 +1,9 @@
+package org.tvheadend.tvhclient.model;
+
+public class ServerInfo {
+    public long id;
+    public int freeDiskSpace;
+    public int totalDiskSpace;
+    public String serverName;
+    public String serverVersion;
+}
