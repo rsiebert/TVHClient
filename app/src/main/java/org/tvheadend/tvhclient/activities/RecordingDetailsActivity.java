926d2bcdd06499e9472f5b65886988dbc2dc14ec
rsiebert <rsiebert80@gmail.com>
2017-12-15 14:30:47 +0100

    Call the recording details activity in single pane mode. It will then show the fragment.

--- app/src/main/java/org/tvheadend/tvhclient/activities/RecordingDetailsActivity.java
+++ app/src/main/java/org/tvheadend/tvhclient/activities/RecordingDetailsActivity.java
@@ -1,50 +1,35 @@
 package org.tvheadend.tvhclient.activities;
 
-import android.annotation.SuppressLint;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
-import android.view.View;
-import android.widget.TextView;
 
 import org.tvheadend.tvhclient.R;
 import org.tvheadend.tvhclient.fragments.recordings.RecordingDetailsFragment;
 import org.tvheadend.tvhclient.interfaces.BackPressedInterface;
-import org.tvheadend.tvhclient.interfaces.ToolbarInterface;
 import org.tvheadend.tvhclient.utils.MiscUtils;
 import org.tvheadend.tvhclient.utils.Utils;
 
-public class RecordingDetailsActivity extends AppCompatActivity implements ToolbarInterface {
-
-    private TextView actionBarTitle;
-    private TextView actionBarSubtitle;
-    private ActionBar actionBar;
+public class RecordingDetailsActivity extends AppCompatActivity implements ToolbarInterfaceLight {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(MiscUtils.getThemeId(this));
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
+        setContentView(R.layout.activity_recording_details);
         Utils.setLanguage(this);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setHomeButtonEnabled(true);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-        // Get the widgets so we can use them later and do not need to inflate again
-        actionBarTitle = toolbar.findViewById(R.id.actionbar_title);
-        actionBarSubtitle = toolbar.findViewById(R.id.actionbar_subtitle);
 
         if (savedInstanceState == null) {
             RecordingDetailsFragment fragment = new RecordingDetailsFragment();
             fragment.setArguments(getIntent().getExtras());
-            getSupportFragmentManager().beginTransaction().add(R.id.main_fragment, fragment).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.content_frame, fragment).commit();
         }
     }
 
@@ -52,7 +37,7 @@ public class RecordingDetailsActivity extends AppCompatActivity implements Toolb
     public void onBackPressed() {
         // If a settings fragment is currently visible, let the fragment
         // handle the back press, otherwise the setting activity.
-        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.main_fragment);
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.content_frame);
         if (fragment != null && fragment instanceof BackPressedInterface) {
             ((BackPressedInterface) fragment).onBackPressed();
         }
@@ -60,36 +45,16 @@ public class RecordingDetailsActivity extends AppCompatActivity implements Toolb
     }
 
     @Override
-    public void setActionBarTitle(final String title) {
-        if (actionBar != null && actionBarTitle != null) {
-            actionBarTitle.setText(title);
-        }
-    }
-
-    @SuppressLint("RtlHardcoded")
-    @Override
-    public void setActionBarSubtitle(final String subtitle) {
-        if (actionBar != null && actionBarSubtitle != null) {
-            actionBarSubtitle.setText(subtitle);
-            // If no subtitle string is given hide it from the view and center
-            // the title vertically, otherwise place it below the title
-            if (subtitle.length() == 0) {
-                actionBarSubtitle.setVisibility(View.GONE);
-                actionBarTitle.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
-            } else {
-                actionBarSubtitle.setVisibility(View.VISIBLE);
-                actionBarTitle.setGravity(Gravity.LEFT | Gravity.BOTTOM);
-            }
+    public void setTitle(String title) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
         }
     }
 
     @Override
-    public void setActionBarIcon(final Bitmap bitmap) {
-        // NOP
-    }
-
-    @Override
-    public void setActionBarIcon(final int resource) {
-        // NOP
+    public void setSubtitle(String subtitle) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setSubtitle(subtitle);
+        }
     }
 }
---
+++ app/src/main/java/org/tvheadend/tvhclient/activities/ToolbarInterfaceLight.java
@@ -0,0 +1,8 @@
+package org.tvheadend.tvhclient.activities;
+
+public interface ToolbarInterfaceLight {
+
+    void setTitle(String title);
+
+    void setSubtitle(String subtitle);
+}
