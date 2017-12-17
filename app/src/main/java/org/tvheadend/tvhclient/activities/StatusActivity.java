87d1ac95a831a5d8eae9a8fb56926315456cd007
rsiebert <rsiebert80@gmail.com>
2017-12-17 21:17:42 +0100

    Added a status activity

---
+++ app/src/main/java/org/tvheadend/tvhclient/activities/StatusActivity.java
@@ -0,0 +1,39 @@
+package org.tvheadend.tvhclient.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import org.tvheadend.tvhclient.R;
+import org.tvheadend.tvhclient.fragments.StatusFragment;
+import org.tvheadend.tvhclient.utils.MiscUtils;
+
+// TODO extend activity that implements the drawer
+
+public class StatusActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setTheme(MiscUtils.getThemeId(this));
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout_light);
+        MiscUtils.setLanguage(this);
+
+        // Setup the action bar and show the title
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setTitle(getString(R.string.status));
+        }
+
+        if (savedInstanceState == null) {
+            StatusFragment fragment = new StatusFragment();
+            fragment.setArguments(getIntent().getExtras());
+            getFragmentManager().beginTransaction().add(R.id.main, fragment).commit();
+        }
+    }
+}
--- app/src/main/java/org/tvheadend/tvhclient/fragments/StatusFragment.java
+++ app/src/main/java/org/tvheadend/tvhclient/fragments/StatusFragment.java
@@ -2,7 +2,7 @@ package org.tvheadend.tvhclient.fragments;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
