d645774176407a97eaa899cc8b6e058a8fca6189
rsiebert <rsiebert80@gmail.com>
2018-02-08 14:21:35 +0100

    Removed unused classes

--- app/src/main/java/org/tvheadend/tvhclient/data/model/DiscSpace.java
+++
@@ -1,6 +0,0 @@
-package org.tvheadend.tvhclient.data.model;
-
-public class DiscSpace {
-    public String freediskspace;
-    public String totaldiskspace;
-}
--- app/src/main/java/org/tvheadend/tvhclient/data/model/SystemTime.java
+++
@@ -1,7 +0,0 @@
-package org.tvheadend.tvhclient.data.model;
-
-public class SystemTime {
-    public String time;
-    public String timezone;
-    public String gmtoffset;
-}
