e3d1c92b233b7ee5b8c17a1ba655c1e3a9b2402a
rsiebert <rsiebert80@gmail.com>
2018-02-20 00:02:06 +0100

    Added database migrations from older app versions. This still needs proper testing.

--- app/src/main/java/org/tvheadend/tvhclient/data/AppDatabase.java
+++ app/src/main/java/org/tvheadend/tvhclient/data/AppDatabase.java
@@ -6,6 +6,7 @@ import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
+import android.database.Cursor;
 import android.support.annotation.NonNull;
 
 import org.tvheadend.tvhclient.data.dao.ChannelDao;
@@ -45,7 +46,7 @@ import org.tvheadend.tvhclient.data.entity.TranscodingProfile;
                 TranscodingProfile.class,
                 ServerStatus.class
         },
-        version = 10)
+        version = 12)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static AppDatabase instance;
@@ -54,19 +55,118 @@ public abstract class AppDatabase extends RoomDatabase {
         if (instance == null) {
             synchronized (AppDatabase.class) {
                 instance = Room.databaseBuilder(context, AppDatabase.class, "tvhclient")
-                        .addMigrations(MIGRATION_6_9)
+                        .addMigrations(MIGRATION_1_2)
+                        .addMigrations(MIGRATION_2_3)
+                        .addMigrations(MIGRATION_3_4)
+                        .addMigrations(MIGRATION_4_5)
+                        .addMigrations(MIGRATION_5_6)
+                        .addMigrations(MIGRATION_6_7)
+                        .addMigrations(MIGRATION_7_8)
+                        .addMigrations(MIGRATION_8_9)
                         .addMigrations(MIGRATION_9_10)
-                        .fallbackToDestructiveMigration()
+                        .addMigrations(MIGRATION_10_11)
+                        .addMigrations(MIGRATION_11_12)
                         .build();
             }
         }
         return instance;
     }
 
-    private static final Migration MIGRATION_6_9 = new Migration(6, 9) {
+    private static final Migration MIGRATION_1_2 = new Migration(1, 2) {
         @Override
         public void migrate(@NonNull SupportSQLiteDatabase database) {
-            database.execSQL("DROP TABLE IF EXISTS profiles");
+            database.execSQL("ALTER TABLE connections ADD COLUMN channel_tag INT DEFAULT 0;");
+        }
+    };
+
+    private static final Migration MIGRATION_2_3 = new Migration(2, 3) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE connections ADD COLUMN streaming_port INT DEFAULT 9981;");
+        }
+    };
+
+    private static final Migration MIGRATION_3_4 = new Migration(3, 4) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            // Add the required columns for WOL
+            database.execSQL("ALTER TABLE connections ADD COLUMN wol_address TEXT NULL;");
+            database.execSQL("ALTER TABLE connections ADD COLUMN wol_port INT DEFAULT 9;");
+        }
+    };
+
+    private static final Migration MIGRATION_4_5 = new Migration(4, 5) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE connections ADD COLUMN wol_broadcast INT DEFAULT 0;");
+        }
+    };
+
+    private static final Migration MIGRATION_5_6 = new Migration(5, 6) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            // Add the id columns for the profiles.
+            database.execSQL("ALTER TABLE connections ADD COLUMN playback_profile_id INT DEFAULT 0;");
+            database.execSQL("ALTER TABLE connections ADD COLUMN recording_profile_id INT DEFAULT 0;");
+            // Add the new profile table
+            final String query = "CREATE TABLE IF NOT EXISTS profiles ("
+                    + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "profile_enabled INT DEFAULT 0, "
+                    + "profile_uuid TEXT NULL, "
+                    + "container TEXT NULL, "
+                    + "transcode INT DEFAULT 0, "
+                    + "resolution TEXT NULL, "
+                    + "acode_codec TEXT NULL, "
+                    + "video_codec TEXT NULL, "
+                    + "subtitle_codec TEXT NULL);";
+            database.execSQL(query);
+        }
+    };
+
+    private static final Migration MIGRATION_6_7 = new Migration(6, 7) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE profiles ADD COLUMN profile_name TEXT NULL;");
+        }
+    };
+
+    private static final Migration MIGRATION_7_8 = new Migration(7, 8) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            // Add the id columns for the profiles but check if they exist
+            Cursor cursor = database.query("SELECT * FROM connections");
+            int colIndex = cursor.getColumnIndex("playback_profile_id");
+            if (colIndex < 0) {
+                database.execSQL("ALTER TABLE connections ADD COLUMN playback_profile_id INT DEFAULT 0;");
+            }
+
+            cursor = database.query("SELECT * FROM connections");
+            colIndex = cursor.getColumnIndex("recording_profile_id");
+            if (colIndex < 0) {
+                database.execSQL("ALTER TABLE connections ADD COLUMN recording_profile_id INT DEFAULT 0;");
+            }
+
+            // Add the new profile table
+            final String query = "CREATE TABLE IF NOT EXISTS profiles ("
+                    + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "profile_enabled INT DEFAULT 0, "
+                    + "profile_uuid TEXT NULL, "
+                    + "profile_name TEXT NULL, "
+                    + "container TEXT NULL, "
+                    + "transcode INT DEFAULT 0, "
+                    + "resolution TEXT NULL, "
+                    + "acode_codec TEXT NULL, "
+                    + "video_codec TEXT NULL, "
+                    + "subtitle_codec TEXT NULL);";
+            database.execSQL(query);
+            cursor.close();
+        }
+    };
+
+    private static final Migration MIGRATION_8_9 = new Migration(8, 9) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE connections ADD COLUMN cast_profile_id INT DEFAULT 0;");
         }
     };
 
@@ -300,6 +400,41 @@ public abstract class AppDatabase extends RoomDatabase {
                     "0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0)");
             // Delete the old connections table
             database.execSQL("DROP TABLE connections_old");
+        }
+    };
+
+    private static final Migration MIGRATION_10_11 = new Migration(10, 11) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE connections ADD COLUMN wol_enabled INTEGER NOT NULL;");
+        }
+    };
+
+    private static final Migration MIGRATION_11_12 = new Migration(11, 12) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+
+            database.execSQL("ALTER TABLE channel_tags RENAME TO channel_tags_old");
+
+            database.execSQL("CREATE TABLE IF NOT EXISTS channel_tags (" +
+                    "id INTEGER NOT NULL, " +
+                    "tag_name TEXT, " +
+                    "tag_index INTEGER NOT NULL, " +
+                    "tag_icon TEXT, " +
+                    "tag_titled_icon INTEGER NOT NULL, " +
+                    "channel_count INTEGER NOT NULL, " +
+                    "PRIMARY KEY(id))");
+
+            // Clone the connection data
+            database.execSQL("INSERT INTO channel_tags (" +
+                    "id, tag_name, tag_index, tag_icon, tag_titled_icon, channel_count) VALUES (" +
+                    "(SELECT id FROM channel_tags_old), " +
+                    "(SELECT tag_name FROM channel_tags_old), " +
+                    "(SELECT tag_index FROM channel_tags_old), " +
+                    "(SELECT tag_icon FROM channel_tags_old), " +
+                    "(SELECT tag_titled_icon FROM channel_tags_old), 0)");
+
+            database.execSQL("DROP TABLE channel_tags_old");
         }
     };
 
