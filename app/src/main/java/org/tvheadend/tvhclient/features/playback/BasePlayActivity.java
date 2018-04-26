543d3d9026831c227b875f51e00125f3bf45f8c3
rsiebert <rsiebert80@gmail.com>
2018-04-26 16:56:59 +0200

    Removed the toolbar from the play activity

--- app/src/main/java/org/tvheadend/tvhclient/features/playback/BasePlayActivity.java
+++ app/src/main/java/org/tvheadend/tvhclient/features/playback/BasePlayActivity.java
@@ -11,9 +11,7 @@ import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -52,15 +50,6 @@ public abstract class BasePlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play_activity);
         MiscUtils.setLanguage(this);
-
-        // Setup the action bar and the toolbar
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setHomeButtonEnabled(true);
-        }
 
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         ConnectionRepository connectionRepository = new ConnectionRepository(this);
