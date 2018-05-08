14f6c34af44eef9c18ecefae17e5a3b8b77e69fd
rsiebert <rsiebert80@gmail.com>
2018-05-09 00:09:36 +0200

    Fixed the suggestion provider

--- app/src/main/java/org/tvheadend/tvhclient/features/search/SuggestionProvider.java
+++ app/src/main/java/org/tvheadend/tvhclient/features/search/SuggestionProvider.java
@@ -3,7 +3,7 @@ package org.tvheadend.tvhclient.features.search;
 import android.content.SearchRecentSuggestionsProvider;
 
 public class SuggestionProvider extends SearchRecentSuggestionsProvider {
-    public final static String AUTHORITY = "org.tvheadend.tvhclient.data.local.SuggestionProvider";
+    public final static String AUTHORITY = "SuggestionProvider";
     public final static int MODE = DATABASE_MODE_QUERIES;
 
     public SuggestionProvider() {
