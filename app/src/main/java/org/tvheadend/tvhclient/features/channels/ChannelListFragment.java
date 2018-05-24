a4213c54ea392156489bc68f6c17146dc13402a0
rsiebert <rsiebert80@gmail.com>
2018-05-24 22:01:18 +0200

    Use the shared preferences from the base class

--- app/src/main/java/org/tvheadend/tvhclient/features/channels/ChannelListFragment.java
+++ app/src/main/java/org/tvheadend/tvhclient/features/channels/ChannelListFragment.java
@@ -3,9 +3,7 @@ package org.tvheadend.tvhclient.features.channels;
 import android.app.SearchManager;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
@@ -146,8 +144,6 @@ public class ChannelListFragment extends BaseFragment implements RecyclerViewCli
 
     @Override
     public void onPrepareOptionsMenu(Menu menu) {
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity);
-
         final boolean showGenreColors = sharedPreferences.getBoolean("genre_colors_for_channels_enabled", false);
         final boolean showChannelTagMenu = sharedPreferences.getBoolean("channel_tag_menu_enabled", true);
 
