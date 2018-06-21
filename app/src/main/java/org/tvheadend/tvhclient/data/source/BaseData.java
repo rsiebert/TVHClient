e5757e3bf3c1309efd6087d5a09776f3250d6b16
rsiebert <rsiebert80@gmail.com>
2018-06-21 13:06:07 +0200

    Make some variables package private

--- app/src/main/java/org/tvheadend/tvhclient/data/source/BaseData.java
+++ app/src/main/java/org/tvheadend/tvhclient/data/source/BaseData.java
@@ -4,11 +4,11 @@ class BaseData {
     protected static final int INSERT = 1;
     protected static final int UPDATE = 2;
     protected static final int DELETE = 3;
-    protected static final int INSERT_ALL = 4;
-    protected static final int DELETE_ALL = 5;
-    protected static final int DELETE_BY_TIME = 6;
-    protected static final int DELETE_BY_ID = 10;
-    protected static final int LOAD_LAST_IN_CHANNEL = 7;
-    protected static final int LOAD_BY_ID = 8;
-    protected static final int LOAD_BY_EVENT_ID = 9;
+    static final int INSERT_ALL = 4;
+    static final int DELETE_ALL = 5;
+    static final int DELETE_BY_TIME = 6;
+    static final int DELETE_BY_ID = 10;
+    static final int LOAD_LAST_IN_CHANNEL = 7;
+    static final int LOAD_BY_ID = 8;
+    static final int LOAD_BY_EVENT_ID = 9;
 }
