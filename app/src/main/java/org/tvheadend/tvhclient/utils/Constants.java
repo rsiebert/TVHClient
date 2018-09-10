fc9ca9f25c9307f0493e0713b7a845249e0995d6
Robert Siebert <rsiebert@miroku.no-ip.com>
2018-09-10 09:55:17 +0200

    Removed unused constants

--- app/src/main/java/org/tvheadend/tvhclient/utils/Constants.java
+++ app/src/main/java/org/tvheadend/tvhclient/utils/Constants.java
@@ -5,21 +5,4 @@ public class Constants {
     // Product id for the in-app billing item to unlock the application
     public static final String UNLOCKER = "unlocker";
 
-    // Codes to identify the returning activity
-    public static final int RESULT_CODE_START_PLAYER = 102;
-
-    // Strings used to identify the information passed via a bundle
-    public static final String BUNDLE_ACTION = "action";
-
-    // Default values for the program guide
-    public static final String EPG_DEFAULT_MAX_DAYS = "7";
-    public static final String EPG_DEFAULT_HOURS_VISIBLE = "4";
-
-    // Channel sorting order
-    public static final int CHANNEL_SORT_DEFAULT = 0;
-    public static final int CHANNEL_SORT_BY_NAME = 1;
-    public static final int CHANNEL_SORT_BY_NUMBER = 2;
-
-    public static final String CAST_PROFILE_DEFAULT = "webtv-vp8-vorbis-webm";
-
 }
