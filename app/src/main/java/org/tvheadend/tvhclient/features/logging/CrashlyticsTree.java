0b1ffca656088fd56dcf6064744ddbb83a908bba
Robert Siebert <rsiebert@miroku.no-ip.com>
2018-09-14 13:38:29 +0200

    Report a crash only if crashlytics is enabled

--- app/src/main/java/org/tvheadend/tvhclient/features/logging/CrashlyticsTree.java
+++ app/src/main/java/org/tvheadend/tvhclient/features/logging/CrashlyticsTree.java
@@ -5,6 +5,8 @@ import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
 
+import io.fabric.sdk.android.Fabric;
+
 public class CrashlyticsTree extends BaseDebugTree {
 
     private static final String CRASHLYTICS_KEY_PRIORITY = "priority";
@@ -16,15 +18,16 @@ public class CrashlyticsTree extends BaseDebugTree {
         if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
             return;
         }
+        if (Fabric.isInitialized()) {
+            Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority);
+            Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag);
+            Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message);
 
-        Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority);
-        Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag);
-        Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message);
-
-        if (t == null) {
-            Crashlytics.logException(new Exception(message));
-        } else {
-            Crashlytics.logException(t);
+            if (t == null) {
+                Crashlytics.logException(new Exception(message));
+            } else {
+                Crashlytics.logException(t);
+            }
         }
     }
 }
