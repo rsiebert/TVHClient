1e8954d0b44c10325f565ff98c3eef71cfe6a806
Robert Siebert <rsiebert@miroku.no-ip.com>
2018-09-23 22:31:33 +0200

    Use join and not left join for getting the active server status.

--- app/src/main/java/org/tvheadend/tvhclient/data/dao/ServerStatusDao.java
+++ app/src/main/java/org/tvheadend/tvhclient/data/dao/ServerStatusDao.java
@@ -18,13 +18,13 @@ public interface ServerStatusDao {
     @Query("SELECT s.*, " +
             "c.name AS connection_name " +
             "FROM server_status AS s " +
-            "LEFT JOIN connections AS c ON c.id = s.connection_id AND c.active = 1")
+            "JOIN connections AS c ON c.id = s.connection_id AND c.active = 1")
     ServerStatus loadServerStatusSync();
 
     @Query("SELECT s.*, " +
             "c.name AS connection_name " +
             "FROM server_status AS s " +
-            "LEFT JOIN connections AS c ON c.id = s.connection_id AND c.active = 1")
+            "JOIN connections AS c ON c.id = s.connection_id AND c.active = 1")
     LiveData<ServerStatus> loadServerStatus();
 
     @Insert
