5dbe2a4f2f71eec6ae13d4f0a569928accb02a30
Robert Siebert <rsiebert@miroku.no-ip.com>
2018-09-26 12:12:28 +0200

    Log all messages through crashlytics in the release build. It will also write to log cat.

--- app/src/main/java/org/tvheadend/tvhclient/features/logging/ReleaseTree.java
+++ app/src/main/java/org/tvheadend/tvhclient/features/logging/ReleaseTree.java
@@ -1,28 +1,14 @@
 package org.tvheadend.tvhclient.features.logging;
 
-import android.util.Log;
+import com.crashlytics.android.Crashlytics;
 
 public class ReleaseTree extends BaseDebugTree {
 
     @Override
-    protected boolean isLoggable(String tag, int priority) {
-        return !(priority == Log.INFO || priority == Log.VERBOSE);
-    }
-
-    @Override
     protected void log(int priority, String tag, String message, Throwable t) {
-        if (isLoggable(tag, priority)) {
-            switch (priority) {
-                case Log.DEBUG:
-                    Log.d(tag, message, t);
-                    break;
-                case Log.WARN:
-                    Log.w(tag, message, t);
-                    break;
-                case Log.ERROR:
-                    Log.e(tag, message, t);
-                    break;
-            }
-        }
+        // Logs the given message when a crash occurred.
+        // In addition to writing to the next crash report, it will also
+        // write to the LogCat using android.util.Log.println(priority, tag, msg)
+        Crashlytics.log(priority, tag, message);
     }
 }
