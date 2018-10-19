e4b6a058d83eed221799c7e21a9d80a1d4437e21
Robert Siebert <rsiebert80@gmail.com>
2018-10-19 15:46:29 +0200

    Added conflict strategy when adding a server status

--- app/src/main/java/org/tvheadend/tvhclient/data/dao/ServerStatusDao.java
+++ app/src/main/java/org/tvheadend/tvhclient/data/dao/ServerStatusDao.java
@@ -4,6 +4,7 @@ import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
@@ -27,7 +28,7 @@ public interface ServerStatusDao {
             "JOIN connections AS c ON c.id = s.connection_id AND c.active = 1")
     LiveData<ServerStatus> loadActiveServerStatus();
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(ServerStatus serverStatus);
 
     @Update
