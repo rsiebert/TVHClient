e7e7dd80e4ee49024e453f9810ffcffe0ccc9146
Robert Siebert <rsiebert80@gmail.com>
2018-10-19 15:45:33 +0200

    Fixed some lint warnings

--- app/src/main/java/org/tvheadend/tvhclient/features/settings/ConnectionViewModel.java
+++ app/src/main/java/org/tvheadend/tvhclient/features/settings/ConnectionViewModel.java
@@ -15,7 +15,6 @@ import javax.inject.Inject;
 
 public class ConnectionViewModel extends AndroidViewModel {
 
-    private final LiveData<List<Connection>> connections;
     @Inject
     protected AppRepository appRepository;
     @Inject
@@ -24,23 +23,22 @@ public class ConnectionViewModel extends AndroidViewModel {
     public ConnectionViewModel(Application application) {
         super(application);
         MainApplication.getComponent().inject(this);
-        connections = appRepository.getConnectionData().getLiveDataItems();
     }
 
     LiveData<List<Connection>> getAllConnections() {
-        return connections;
+        return appRepository.getConnectionData().getLiveDataItems();
     }
 
-    public void setConnectionHasChanged(boolean change) {
+    void setConnectionHasChanged(boolean change) {
         sharedPreferences.edit().putBoolean("connection_value_changed", change).apply();
     }
 
-    public int getActiveConnectionId() {
+    int getActiveConnectionId() {
         Connection connection = appRepository.getConnectionData().getActiveItem();
         return (connection != null ? connection.getId() : -1);
     }
 
-    public boolean getConnectionHasChanged() {
+    boolean getConnectionHasChanged() {
         return sharedPreferences.getBoolean("connection_value_changed", false);
     }
 }
