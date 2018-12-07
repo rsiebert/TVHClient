70ee81d64e771a57a60059ba4e483bf25e5ab84b
Robert Siebert <rsiebert80@gmail.com>
2018-12-07 14:50:22 +0100

    Minimize the required UI updates when a recording is being recorded

--- app/src/main/java/org/tvheadend/tvhclient/features/epg/EpgProgramListRecyclerViewAdapter.java
+++ app/src/main/java/org/tvheadend/tvhclient/features/epg/EpgProgramListRecyclerViewAdapter.java
@@ -3,6 +3,7 @@ package org.tvheadend.tvhclient.features.epg;
 import android.support.annotation.NonNull;
 import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -74,8 +75,17 @@ class EpgProgramListRecyclerViewAdapter extends RecyclerView.Adapter<EpgProgramL
 
             for (Recording recording : recordings) {
                 if (program.getEventId() == recording.getEventId()) {
+                    Recording oldRecording = program.getRecording();
                     program.setRecording(recording);
-                    notifyItemChanged(i);
+
+                    // Do a full update only when a new recording was added or the recording
+                    // state has changed which results in a different recording state icon
+                    // Otherwise do not update the UI
+                    if (oldRecording == null
+                            || (!TextUtils.equals(oldRecording.getError(), recording.getError())
+                            || !TextUtils.equals(oldRecording.getState(), recording.getState()))) {
+                        notifyItemChanged(i);
+                    }
                     recordingExists = true;
                     break;
                 }
