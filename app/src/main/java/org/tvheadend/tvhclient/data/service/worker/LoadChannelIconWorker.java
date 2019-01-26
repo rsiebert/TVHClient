155dd42eed7b59cd1b433bae7fc645ecd6e8fb2f
Robert Siebert <rsiebert80@gmail.com>
2019-01-26 07:32:35 +0100

    Added worker to load channel icons after sync in the background

---
+++ app/src/main/java/org/tvheadend/tvhclient/data/service/worker/LoadChannelIconWorker.java
@@ -0,0 +1,26 @@
+package org.tvheadend.tvhclient.data.service.worker;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+
+import org.tvheadend.tvhclient.data.service.EpgSyncIntentService;
+
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+import timber.log.Timber;
+
+public class LoadChannelIconWorker extends Worker {
+
+    public LoadChannelIconWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Timber.d("Loading channel icons from server");
+        EpgSyncIntentService.enqueueWork(getApplicationContext(), new Intent().setAction("loadChannelIcons"));
+        return Result.success();
+    }
+}
