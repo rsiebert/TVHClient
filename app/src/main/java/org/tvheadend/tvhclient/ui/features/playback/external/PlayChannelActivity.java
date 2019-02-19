7cd5c21573f6c65cef4a60049a2e28cd6a44afe8
Robert Siebert <rsiebert80@gmail.com>
2019-02-19 11:27:24 +0100

    Renamed the streaming package to playback

---
+++ app/src/main/java/org/tvheadend/tvhclient/ui/features/playback/external/PlayChannelActivity.java
@@ -0,0 +1,71 @@
+package org.tvheadend.tvhclient.ui.features.playback.external;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+
+import org.tvheadend.tvhclient.R;
+import org.tvheadend.tvhclient.domain.entity.Channel;
+import org.tvheadend.tvhclient.data.service.HtspService;
+
+import androidx.annotation.Nullable;
+import timber.log.Timber;
+
+public class PlayChannelActivity extends BasePlaybackActivity {
+
+    private int channelId;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            channelId = getIntent().getIntExtra("channelId", -1);
+        } else {
+            Bundle bundle = getIntent().getExtras();
+            if (bundle != null) {
+                channelId = bundle.getInt("channelId", -1);
+            }
+        }
+    }
+
+    @Override
+    protected boolean requireHostnameToAddressConversion() {
+        return false;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("channelId", channelId);
+    }
+
+    @Override
+    protected void getHttpTicket() {
+        if (channelId > 0) {
+            Intent intent = new Intent(this, HtspService.class);
+            intent.setAction("getTicket");
+            intent.putExtra("channelId", channelId);
+            startService(intent);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            statusTextView.setText(getString(R.string.error_starting_playback_no_channel));
+        }
+    }
+
+    @Override
+    protected void onHttpTicketReceived() {
+        Channel channel = appRepository.getChannelData().getItemById(channelId);
+
+        Timber.d("Playing channel from server with url " + serverUrl);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.parse(serverUrl), "video/*");
+        if (!TextUtils.isEmpty(channel.getName())) {
+            intent.putExtra("itemTitle", channel.getName());
+            intent.putExtra("title", channel.getName());
+        }
+        startExternalPlayer(intent);
+    }
+}
