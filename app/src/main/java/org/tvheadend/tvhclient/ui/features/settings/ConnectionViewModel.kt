72a8319ce73fade92ee5d82d1ee9f5ccf6ebcc16
Robert Siebert <rsiebert80@gmail.com>
2019-03-05 17:06:59 +0100

    Converted the connection view model to kotlin

---
+++ app/src/main/java/org/tvheadend/tvhclient/ui/features/settings/ConnectionViewModel.kt
@@ -0,0 +1,36 @@
+package org.tvheadend.tvhclient.ui.features.settings
+
+import android.app.Application
+import android.content.SharedPreferences
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import org.tvheadend.tvhclient.MainApplication
+import org.tvheadend.tvhclient.data.repository.AppRepository
+import org.tvheadend.tvhclient.domain.entity.Connection
+import javax.inject.Inject
+
+class ConnectionViewModel(application: Application) : AndroidViewModel(application) {
+
+    @Inject
+    lateinit var appRepository: AppRepository
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
+    val allConnections: LiveData<List<Connection>>
+
+    val activeConnectionId: Int
+        get() {
+            val connection = appRepository.connectionData.activeItem
+            return connection?.id ?: -1
+        }
+
+    var connectionHasChanged: Boolean
+        get() = sharedPreferences.getBoolean("connection_value_changed", false)
+        set(change) = sharedPreferences.edit().putBoolean("connection_value_changed", change).apply()
+
+    init {
+        MainApplication.getComponent().inject(this)
+        allConnections = appRepository.connectionData.getLiveDataItems() ?: MutableLiveData()
+    }
+}
