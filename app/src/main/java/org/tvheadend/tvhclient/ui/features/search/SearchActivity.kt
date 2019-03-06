cc934b71642d872293503f13bb8883d5812baddd
Robert Siebert <rsiebert80@gmail.com>
2019-03-06 17:23:26 +0100

    Listen for network changes in the search activity to enable the recording menus when a program is selected

--- app/src/main/java/org/tvheadend/tvhclient/ui/features/search/SearchActivity.kt
+++ app/src/main/java/org/tvheadend/tvhclient/ui/features/search/SearchActivity.kt
@@ -2,16 +2,23 @@ package org.tvheadend.tvhclient.ui.features.search
 
 import android.app.SearchManager
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.provider.SearchRecentSuggestions
 import androidx.fragment.app.Fragment
 import org.tvheadend.tvhclient.R
 import org.tvheadend.tvhclient.ui.base.BaseActivity
+import org.tvheadend.tvhclient.ui.base.callbacks.NetworkStatusListener
+import org.tvheadend.tvhclient.ui.base.utils.SnackbarUtils
 import org.tvheadend.tvhclient.ui.features.programs.ProgramListFragment
 import org.tvheadend.tvhclient.util.MiscUtils
+import org.tvheadend.tvhclient.util.network.NetworkStatusReceiver
 import timber.log.Timber
 
-class SearchActivity : BaseActivity(), StartSearchInterface {
+class SearchActivity : BaseActivity(), StartSearchInterface, NetworkStatusListener {
+
+    private var networkStatusReceiver: NetworkStatusReceiver? = null
+    private var isNetworkAvailable: Boolean = false
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(MiscUtils.getThemeId(this))
@@ -20,6 +27,8 @@ class SearchActivity : BaseActivity(), StartSearchInterface {
 
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        networkStatusReceiver = NetworkStatusReceiver(this)
 
         handleIntent(intent)
 
@@ -65,4 +74,53 @@ class SearchActivity : BaseActivity(), StartSearchInterface {
             suggestions.saveRecentQuery(query, null)
         }
     }
+
+    public override fun onStart() {
+        super.onStart()
+        registerReceiver(networkStatusReceiver, IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"))
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        unregisterReceiver(networkStatusReceiver)
+    }
+
+    override fun onNetworkStatusChanged(isNetworkAvailable: Boolean) {
+        onNetworkAvailabilityChanged(isNetworkAvailable)
+        if (!isNetworkAvailable) {
+            SnackbarUtils.sendSnackbarMessage(this, "No network available")
+        }
+    }
+
+    /**
+     * Executes certain actions when the connectivity has changed.
+     * A new connection to the server is created if the connectivity changed from
+     * unavailable to available. Otherwise the server will be pinged to check if the connection
+     * is still active. Additionally the connectivity status is propagated to all fragments that
+     * that are currently shown so they can update certain UI elements that depend on the
+     * connectivity status like menus.
+     *
+     * @param isAvailable True if networking is available, otherwise false
+     */
+    private fun onNetworkAvailabilityChanged(isAvailable: Boolean) {
+        Timber.d("Network availability changed, network is available $isAvailable")
+        isNetworkAvailable = isAvailable
+
+        var fragment = supportFragmentManager.findFragmentById(R.id.main)
+        if (fragment is NetworkStatusListener) {
+            (fragment as NetworkStatusListener).onNetworkStatusChanged(isAvailable)
+        }
+
+        fragment = supportFragmentManager.findFragmentById(R.id.details)
+        if (fragment is NetworkStatusListener) {
+            (fragment as NetworkStatusListener).onNetworkStatusChanged(isAvailable)
+        }
+        Timber.d("Network availability changed, invalidating menu")
+        invalidateOptionsMenu()
+    }
+
+    override fun isNetworkAvailable(): Boolean {
+        return isNetworkAvailable
+    }
+
 }
