95ae10f931a030464a7afdadeb4ec33e8d9e35f5
Robert Siebert <rsiebert80@gmail.com>
2019-03-11 16:05:50 +0100

    Converted the date and time picker fragments to kotlin

---
+++ app/src/main/java/org/tvheadend/tvhclient/ui/features/dvr/DatePickerFragment.kt
@@ -0,0 +1,47 @@
+package org.tvheadend.tvhclient.ui.features.dvr
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+import java.util.*
+
+class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    private var hourOfDay: Int = 0
+    private var minute: Int = 0
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val c = Calendar.getInstance()
+        // Get the given milliseconds, if non are given then the current time is used
+        c.timeInMillis = arguments?.getLong("milliSeconds") ?: 0
+
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        hourOfDay = c.get(Calendar.HOUR_OF_DAY)
+        minute = c.get(Calendar.MINUTE)
+
+        return DatePickerDialog(context!!, this, year, month, day)
+    }
+
+    override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.YEAR, year)
+        calendar.set(Calendar.MONTH, month)
+        calendar.set(Calendar.DAY_OF_MONTH, day)
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+        calendar.set(Calendar.MINUTE, minute)
+
+        val fragment = targetFragment
+        if (fragment is Listener) {
+            fragment.onDateSelected(calendar.timeInMillis, tag)
+        }
+    }
+
+    interface Listener {
+        fun onDateSelected(milliSeconds: Long, tag: String?)
+    }
+}
