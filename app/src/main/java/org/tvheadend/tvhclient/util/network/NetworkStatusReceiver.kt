8556ebb924dcdd0436280a690304ba32c6ca0e6e
Robert Siebert <rsiebert80@gmail.com>
2019-03-18 12:16:45 +0100

    Converted networking related classes to kotlin

---
+++ app/src/main/java/org/tvheadend/tvhclient/util/network/NetworkStatusReceiver.kt
@@ -0,0 +1,21 @@
+package org.tvheadend.tvhclient.util.network
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+import org.tvheadend.tvhclient.MainApplication
+import org.tvheadend.tvhclient.ui.base.callbacks.NetworkStatusListener
+
+import java.lang.ref.WeakReference
+
+class NetworkStatusReceiver(callback: NetworkStatusListener) : BroadcastReceiver() {
+
+    private val callback: WeakReference<NetworkStatusListener> = WeakReference(callback)
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (MainApplication.isActivityVisible()) {
+            (callback.get() as NetworkStatusListener).onNetworkStatusChanged(isConnectionAvailable(context))
+        }
+    }
+}
---
+++ app/src/main/java/org/tvheadend/tvhclient/util/network/NetworkUtils.kt
@@ -0,0 +1,39 @@
+@file:JvmName("NetworkUtils")
+
+package org.tvheadend.tvhclient.util.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.wifi.WifiManager
+import timber.log.Timber
+import java.lang.reflect.InvocationTargetException
+
+fun isConnectionAvailable(context: Context): Boolean {
+    return isNetworkAvailable(context) || isWifiApEnabled(context)
+}
+
+fun isNetworkAvailable(context: Context): Boolean {
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val activeNetworkInfo = connectivityManager.activeNetworkInfo
+    return activeNetworkInfo != null && activeNetworkInfo.isConnected
+}
+
+private fun isWifiApEnabled(context: Context): Boolean {
+    val wifiManager = context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+    val wmMethods = wifiManager.javaClass.declaredMethods
+    for (method in wmMethods) {
+        if (method.name == "isWifiApEnabled") {
+            try {
+                return method.invoke(wifiManager) as Boolean
+            } catch (e: IllegalArgumentException) {
+                Timber.d("Illegal argument error while invoking method 'isWifiApEnabled'", e)
+            } catch (e: IllegalAccessException) {
+                Timber.d("Illegal access error while invoking method 'isWifiApEnabled'", e)
+            } catch (e: InvocationTargetException) {
+                Timber.d("Invocation error while invoking method 'isWifiApEnabled'", e)
+            }
+        }
+    }
+    return false
+}
+
