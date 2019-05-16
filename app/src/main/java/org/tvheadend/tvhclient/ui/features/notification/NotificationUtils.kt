75c2676747afeda1877164553d3b316f96051b29
Robert Siebert <rsiebert80@gmail.com>
2019-05-16 21:57:31 +0200

    Added method to show permanent notifications

--- app/src/main/java/org/tvheadend/tvhclient/ui/features/notification/NotificationUtils.kt
+++ app/src/main/java/org/tvheadend/tvhclient/ui/features/notification/NotificationUtils.kt
@@ -6,10 +6,9 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
-import androidx.annotation.RequiresApi
-import androidx.preference.PreferenceManager
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.preference.PreferenceManager
 import androidx.work.Data
 import androidx.work.ExistingWorkPolicy
 import androidx.work.OneTimeWorkRequest
@@ -140,19 +139,37 @@ fun addNotification(context: Context, program: ProgramInterface, profile: Server
     }
 }
 
-@RequiresApi(Build.VERSION_CODES.M)
-fun addPermanentNotification(context: Context, count: Int) {
-    (context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).activeNotifications.forEach { notification ->
-        if (notification.id == 1) {
-            Timber.d("Notification exists already, skipping")
-            return
+fun addRunningRecordingNotification(context: Context, count: Int) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        (context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).activeNotifications.forEach { notification ->
+            if (notification.id == 1) {
+                Timber.d("Notification exists already, skipping")
+                return
+            }
         }
-    }
 
-    val builder = getNotificationBuilder(context)
-    builder.setContentTitle(context.getString(R.string.currently_recording))
-            .setContentText("$count recording are running")
-            .setSmallIcon(R.drawable.ic_menu_record_dark)
-            .setOngoing(true)
-    NotificationManagerCompat.from(context).notify(1, builder.build())
+        val builder = getNotificationBuilder(context)
+        builder.setContentTitle(context.getString(R.string.currently_recording))
+                .setContentText("$count recording are running")
+                .setSmallIcon(R.drawable.ic_menu_record_dark)
+                .setOngoing(true)
+        NotificationManagerCompat.from(context).notify(1, builder.build())
+    }
+}
+
+fun addDiskSpaceLowNotification(context: Context, gigabytes: Int) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        (context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).activeNotifications.forEach { notification ->
+            if (notification.id == 2) {
+                Timber.d("Notification exists already, skipping")
+                return
+            }
+        }
+
+        val builder = getNotificationBuilder(context)
+        builder.setContentTitle(context.getString(R.string.disc_space))
+                .setContentText("Disk space is running low, $gigabytes remaining")
+                .setSmallIcon(R.drawable.ic_menu_info_dark)
+        NotificationManagerCompat.from(context).notify(2, builder.build())
+    }
 }
