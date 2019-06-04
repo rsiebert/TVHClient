f93e0716ec784ce16f247383cfdcfdf5ea41d835
Robert Siebert <rsiebert80@gmail.com>
2019-06-04 11:25:39 +0200

    Removed some copyright information in certain files

--- app/src/main/java/org/tvheadend/tvhclient/ui/features/settings/SettingsProfilesFragment.kt
+++ app/src/main/java/org/tvheadend/tvhclient/ui/features/settings/SettingsProfilesFragment.kt
@@ -1,21 +1,3 @@
-/*
- *  Copyright (C) 2013 Robert Siebert
- *
- * This file is part of TVHGuide.
- *
- * TVHGuide is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * TVHGuide is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with TVHGuide.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.tvheadend.tvhclient.ui.features.settings
 
 import android.os.Bundle
