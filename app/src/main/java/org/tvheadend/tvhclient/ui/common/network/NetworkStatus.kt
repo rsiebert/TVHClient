28238bdaf86d433ca3f4543be4ef0fc45dc0cd7f
Robert Siebert <rsiebert80@gmail.com>
2019-06-18 10:25:01 +0200

    Removed some classes

--- app/src/main/java/org/tvheadend/tvhclient/ui/common/network/NetworkStatus.kt
+++
@@ -1,9 +0,0 @@
-package org.tvheadend.tvhclient.ui.common.network
-
-enum class NetworkStatus {
-    NETWORK_UNKNOWN,
-    NETWORK_IS_DOWN,
-    NETWORK_IS_UP,
-    NETWORK_IS_STILL_DOWN,
-    NETWORK_IS_STILL_UP
-}
