7c3263c5a4d25d7c2109c92e43fe701e48e3652d
Robert Siebert <rsiebert80@gmail.com>
2019-06-27 13:40:30 +0200

    Update the data in the repository, not in the viewmodel

--- app/src/main/java/org/tvheadend/tvhclient/ui/common/NetworkStatusReceiver.kt
+++ app/src/main/java/org/tvheadend/tvhclient/ui/common/NetworkStatusReceiver.kt
@@ -3,16 +3,14 @@ package org.tvheadend.tvhclient.ui.common
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import org.tvheadend.tvhclient.ui.common.getNetworkStatus
-import org.tvheadend.tvhclient.ui.common.isConnectionAvailable
-import org.tvheadend.tvhclient.ui.features.MainViewModel
+import org.tvheadend.tvhclient.data.repository.AppRepository
 import timber.log.Timber
 
-class NetworkStatusReceiver(private val mainViewModel: MainViewModel) : BroadcastReceiver() {
+class NetworkStatusReceiver(private val appRepository: AppRepository) : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         val isAvailable = isConnectionAvailable(context)
         Timber.d("Network availability is $isAvailable")
-        mainViewModel.networkStatus.value = getNetworkStatus(mainViewModel.networkStatus.value, isAvailable)
+        appRepository.networkStatus.value = getNetworkStatus(appRepository.networkStatus.value, isAvailable)
     }
 }
--- app/src/main/java/org/tvheadend/tvhclient/ui/common/SnackbarMessageReceiver.kt
+++ app/src/main/java/org/tvheadend/tvhclient/ui/common/SnackbarMessageReceiver.kt
@@ -3,24 +3,24 @@ package org.tvheadend.tvhclient.ui.common
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import org.tvheadend.tvhclient.ui.features.MainViewModel
+import org.tvheadend.tvhclient.data.repository.AppRepository
 import timber.log.Timber
 
 /**
  * This receiver handles the data that was given from an intent via the LocalBroadcastManager.
  * Any string in the given extra will be shown as a snackbar.
  */
-class SnackbarMessageReceiver(private val mainViewModel: MainViewModel) : BroadcastReceiver() {
+class SnackbarMessageReceiver(private val appRepository: AppRepository) : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         Timber.d("Received new snackbar message")
-        mainViewModel.showSnackbar.value = intent
+        appRepository.snackbarMessage.value = intent
     }
 
     companion object {
 
-        const val ACTION = "snackbar_message"
-        const val CONTENT = "content"
-        const val DURATION = "duration"
+        const val SNACKBAR_ACTION = "snackbar_message"
+        const val SNACKBAR_CONTENT = "content"
+        const val SNACKBAR_DURATION = "duration"
     }
 }
