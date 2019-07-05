e9f88da485ff3915a31744121b58d448e00fa4f8
Robert Siebert <rsiebert80@gmail.com>
2019-07-05 11:44:04 +0200

    Refactored the download manager

--- app/src/main/java/org/tvheadend/tvhclient/ui/features/download/DownloadRecordingManager.kt
+++ app/src/main/java/org/tvheadend/tvhclient/ui/features/download/DownloadRecordingManager.kt
@@ -19,54 +19,58 @@ import org.tvheadend.tvhclient.R
 import org.tvheadend.tvhclient.domain.entity.Connection
 import org.tvheadend.tvhclient.domain.entity.Recording
 import org.tvheadend.tvhclient.domain.entity.ServerStatus
-import org.tvheadend.tvhclient.ui.common.SnackbarMessageReceiver
+import org.tvheadend.tvhclient.ui.common.SnackbarMessageReceiver.Companion.SNACKBAR_ACTION
+import org.tvheadend.tvhclient.ui.common.SnackbarMessageReceiver.Companion.SNACKBAR_CONTENT
+import org.tvheadend.tvhclient.ui.common.SnackbarMessageReceiver.Companion.SNACKBAR_DURATION
 import timber.log.Timber
 
-class DownloadRecordingManager(private val activity: Activity, private val connection: Connection, private val serverStatus: ServerStatus, private val recording: Recording) {
+class DownloadRecordingManager(private val activity: Activity?, private val connection: Connection, private val serverStatus: ServerStatus, private val recording: Recording?) {
 
-    private val downloadManager: DownloadManager
+    private lateinit var downloadManager: DownloadManager
     private var lastDownloadId: Long = 0
 
     init {
-        Timber.d("Initializing download manager, given recording id is ${recording.id}")
-        downloadManager = activity.getSystemService(Service.DOWNLOAD_SERVICE) as DownloadManager
-        if (isStoragePermissionGranted) {
-            startDownload()
+        if (activity != null) {
+            Timber.d("Initializing download manager, given recording id is ${recording?.id}")
+            downloadManager = activity.getSystemService(Service.DOWNLOAD_SERVICE) as DownloadManager
+            if (recording != null && getIsStoragePermissionGranted(activity)) {
+                startDownload(activity, recording)
+            }
         }
     }
 
-    private val downloadRequest: DownloadManager.Request
-        get() {
-            // The path that can be specified can only be in the external storage.
-            // Therefore /storage/emulated/0 is fixed, only the location within this folder can be changed
-            val downloadDirectory = PreferenceManager.getDefaultSharedPreferences(activity)
-                    .getString("download_directory", Environment.DIRECTORY_DOWNLOADS)
-            val downloadUrl = "http://" +
-                    connection.hostname + ":" +
-                    connection.streamingPort +
-                    (serverStatus.webroot ?: "") +
-                    "/dvrfile/" +
-                    recording.id
-            // The user and password are required for authentication. They need to be encoded.
-            val credentials = "Basic " + Base64.encodeToString((connection.username + ":" + connection.password).toByteArray(), Base64.NO_WRAP)
-            // Use the recording title if present, otherwise use the recording id only
-            val recordingTitle = (if (!recording.title.isNullOrEmpty()) recording.title?.replace(" ", "_") else recording.id.toString()) + ".mkv"
+    private fun getDownloadRequest(recording: Recording): DownloadManager.Request {
 
-            Timber.d("Download recording from url $downloadUrl to $downloadDirectory/$recordingTitle")
-            val request = DownloadManager.Request(Uri.parse(downloadUrl))
-                    .addRequestHeader("Authorization", credentials)
-                    .setTitle(recording.title)
-                    .setDescription(recording.description)
-                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        // The path that can be specified can only be in the external storage.
+        // Therefore /storage/emulated/0 is fixed, only the location within this folder can be changed
+        val downloadDirectory = PreferenceManager.getDefaultSharedPreferences(activity)
+                .getString("download_directory", Environment.DIRECTORY_DOWNLOADS)
+        val downloadUrl = "http://" +
+                connection.hostname + ":" +
+                connection.streamingPort +
+                (serverStatus.webroot ?: "") +
+                "/dvrfile/" +
+                recording.id
+        // The user and password are required for authentication. They need to be encoded.
+        val credentials = "Basic " + Base64.encodeToString((connection.username + ":" + connection.password).toByteArray(), Base64.NO_WRAP)
+        // Use the recording title if present, otherwise use the recording id only
+        val recordingTitle = (if (!recording.title.isNullOrEmpty()) recording.title?.replace(" ", "_") else recording.id.toString()) + ".mkv"
 
-            try {
-                request.setDestinationInExternalPublicDir(downloadDirectory, recordingTitle)
-            } catch (e: IllegalStateException) {
-                Timber.d(e, "Could not set download destination directory to $downloadDirectory, falling back to default ${Environment.DIRECTORY_DOWNLOADS}")
-                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, recordingTitle)
-            }
-            return request
+        Timber.d("Download recording from serverUrl $downloadUrl to $downloadDirectory/$recordingTitle")
+        val request = DownloadManager.Request(Uri.parse(downloadUrl))
+                .addRequestHeader("Authorization", credentials)
+                .setTitle(recording.title)
+                .setDescription(recording.description)
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+
+        try {
+            request.setDestinationInExternalPublicDir(downloadDirectory, recordingTitle)
+        } catch (e: IllegalStateException) {
+            Timber.d(e, "Could not set download destination directory to $downloadDirectory, falling back to default ${Environment.DIRECTORY_DOWNLOADS}")
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, recordingTitle)
         }
+        return request
+    }
 
     /**
      * Android API version 23 and higher requires that the permission to access
@@ -76,23 +80,22 @@ class DownloadRecordingManager(private val activity: Activity, private val conne
      *
      * @return True if permission is granted, otherwise false
      */
-    private val isStoragePermissionGranted: Boolean
-        get() {
-            Timber.d("Checking if storage permission was granted")
-            return if (Build.VERSION.SDK_INT >= 23) {
-                if (activity.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                    Timber.d("Storage permissions were granted (API >= 23)")
-                    true
-                } else {
-                    Timber.d("Storage permissions are not yet granted (API >= 23)")
-                    ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
-                    false
-                }
-            } else {
-                Timber.d("Storage permissions were granted (API < 23)")
+    private fun getIsStoragePermissionGranted(activity: Activity): Boolean {
+        Timber.d("Checking if storage permission was granted")
+        return if (Build.VERSION.SDK_INT >= 23) {
+            if (activity.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Timber.d("Storage permissions were granted (API >= 23)")
                 true
+            } else {
+                Timber.d("Storage permissions are not yet granted (API >= 23)")
+                ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+                false
             }
+        } else {
+            Timber.d("Storage permissions were granted (API < 23)")
+            true
         }
+    }
 
     /**
      * Creates the request for the download and puts the request with the
@@ -102,20 +105,20 @@ class DownloadRecordingManager(private val activity: Activity, private val conne
      * required because the download manager does not throw this error via a
      * notification.
      */
-    private fun startDownload() {
+    private fun startDownload(activity: Activity, recording: Recording) {
         Timber.d("Starting download of recording ${recording.title}")
 
 
-        lastDownloadId = downloadManager.enqueue(downloadRequest)
+        lastDownloadId = downloadManager.enqueue(getDownloadRequest(recording))
         Timber.d("Started download with id $lastDownloadId")
 
         // Check after a certain delay the status of the download and that for
         // example the download has not failed due to insufficient storage space.
         // The download manager does not sent a broadcast if this error occurs.
-        Handler().postDelayed({ this.showDownloadStatusMessage() }, 1500)
+        Handler().postDelayed({ this.showDownloadStatusMessage(activity, recording) }, 1500)
     }
 
-    private fun showDownloadStatusMessage() {
+    private fun showDownloadStatusMessage(activity: Activity, recording: Recording) {
         Timber.d("Checking download status of recording ${recording.title}")
 
         // Initialize the default status message
@@ -154,9 +157,9 @@ class DownloadRecordingManager(private val activity: Activity, private val conne
         }
 
         Timber.d("Download status of recording ${recording.title} is $msg")
-        val intent = Intent(SnackbarMessageReceiver.ACTION)
-        intent.putExtra(SnackbarMessageReceiver.CONTENT, msg)
-        intent.putExtra(SnackbarMessageReceiver.DURATION, Snackbar.LENGTH_LONG)
+        val intent = Intent(SNACKBAR_ACTION)
+        intent.putExtra(SNACKBAR_CONTENT, msg)
+        intent.putExtra(SNACKBAR_DURATION, Snackbar.LENGTH_LONG)
         LocalBroadcastManager.getInstance(activity).sendBroadcast(intent)
     }
 }
