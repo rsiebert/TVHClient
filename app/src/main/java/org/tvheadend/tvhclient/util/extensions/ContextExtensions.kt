9a1fb3355da27f4733fda0973514ce18fe300906
Robert Siebert <rsiebert80@gmail.com>
2019-08-06 14:14:36 +0200

    Support defining the snackbar duration

--- app/src/main/java/org/tvheadend/tvhclient/util/extensions/ContextExtensions.kt
+++ app/src/main/java/org/tvheadend/tvhclient/util/extensions/ContextExtensions.kt
@@ -3,16 +3,18 @@ package org.tvheadend.tvhclient.util.extensions
 import android.content.Context
 import android.content.Intent
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.google.android.material.snackbar.Snackbar
 import org.tvheadend.tvhclient.ui.common.SnackbarMessageReceiver
 import timber.log.Timber
 
-fun Context.sendSnackbarMessage(resId: Int) {
-    this.sendSnackbarMessage(this.getString(resId))
+fun Context.sendSnackbarMessage(resId: Int, duration: Int = Snackbar.LENGTH_SHORT) {
+    this.sendSnackbarMessage(this.getString(resId), duration)
 }
 
-fun Context.sendSnackbarMessage(msg: String) {
+fun Context.sendSnackbarMessage(msg: String, duration: Int = Snackbar.LENGTH_SHORT) {
     Timber.d("Sending broadcast to show snackbar message $msg")
     val intent = Intent(SnackbarMessageReceiver.SNACKBAR_ACTION)
     intent.putExtra(SnackbarMessageReceiver.SNACKBAR_CONTENT, msg)
+    intent.putExtra(SnackbarMessageReceiver.SNACKBAR_DURATION, duration)
     LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
 }
