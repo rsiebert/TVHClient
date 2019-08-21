7d5b1c86bb7fc19556a82f86114a893385e3d2ff
Robert Siebert <rsiebert80@gmail.com>
2019-08-21 15:04:17 +0200

    Removed unneeded call to configure the snackbar The material theme does it for us

--- app/src/main/java/org/tvheadend/tvhclient/ui/common/SnackbarUtils.kt
+++ app/src/main/java/org/tvheadend/tvhclient/ui/common/SnackbarUtils.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import org.tvheadend.tvhclient.ui.common.SnackbarMessageReceiver.Companion.SNACKBAR_CONTENT
 import org.tvheadend.tvhclient.ui.common.SnackbarMessageReceiver.Companion.SNACKBAR_DURATION
-import org.tvheadend.tvhclient.util.extensions.config
 import timber.log.Timber
 
 fun showSnackbarMessage(activity: AppCompatActivity, intent: Intent) {
@@ -15,8 +14,6 @@ fun showSnackbarMessage(activity: AppCompatActivity, intent: Intent) {
     val view: View? = activity.findViewById(android.R.id.content)
     view?.let {
         Timber.d("Showing snackbar message $msg")
-        val snackbar = Snackbar.make(view, msg, duration)
-        snackbar.config(activity)
-        snackbar.show()
+        Snackbar.make(view, msg, duration).show()
     }
 }
