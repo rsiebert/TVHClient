edfcfaf1ab816a1add91fc55b16deb24a04b0962
Robert Siebert <rsiebert80@gmail.com>
2019-10-08 14:58:25 +0200

    Use non nullable strings

--- app/src/main/java/org/tvheadend/tvhclient/ui/common/LocaleUtils.kt
+++ app/src/main/java/org/tvheadend/tvhclient/ui/common/LocaleUtils.kt
@@ -14,21 +14,22 @@ fun onAttach(context: Context): Context {
     return setLocale(context, lang)
 }
 
-private fun setLocale(context: Context, language: String?): Context {
+private fun setLocale(context: Context, language: String): Context {
     persist(context, language)
 
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
         updateResources(context, language)
-    } else updateResourcesLegacy(context, language)
-
+    } else {
+        updateResourcesLegacy(context, language)
+    }
 }
 
-private fun getPersistedData(context: Context, defaultLanguage: String): String? {
+private fun getPersistedData(context: Context, defaultLanguage: String): String {
     val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-    return preferences.getString(SELECTED_LANGUAGE, defaultLanguage)
+    return preferences.getString(SELECTED_LANGUAGE, defaultLanguage) ?: defaultLanguage
 }
 
-private fun persist(context: Context, language: String?) {
+private fun persist(context: Context, language: String) {
     val preferences = PreferenceManager.getDefaultSharedPreferences(context)
     val editor = preferences.edit()
 
@@ -37,7 +38,7 @@ private fun persist(context: Context, language: String?) {
 }
 
 @TargetApi(Build.VERSION_CODES.N)
-private fun updateResources(context: Context, language: String?): Context {
+private fun updateResources(context: Context, language: String): Context {
     val locale = Locale(language)
     Locale.setDefault(locale)
 
@@ -48,7 +49,7 @@ private fun updateResources(context: Context, language: String?): Context {
     return context.createConfigurationContext(configuration)
 }
 
-private fun updateResourcesLegacy(context: Context, language: String?): Context {
+private fun updateResourcesLegacy(context: Context, language: String): Context {
     val locale = Locale(language)
     Locale.setDefault(locale)
     val resources = context.resources
