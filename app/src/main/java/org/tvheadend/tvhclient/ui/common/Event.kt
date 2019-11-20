050adab3cd27f11bacd0c6bbecbe6d051c25a28b
Robert Siebert <rsiebert80@gmail.com>
2019-11-20 21:49:01 +0100

    Suppress a warning about an unused method

--- app/src/main/java/org/tvheadend/tvhclient/ui/common/Event.kt
+++ app/src/main/java/org/tvheadend/tvhclient/ui/common/Event.kt
@@ -23,5 +23,6 @@ open class Event<out T>(private val content: T) {
     /**
      * Returns the content, even if it's already been handled.
      */
+    @Suppress("unused")
     fun peekContent(): T = content
 }
