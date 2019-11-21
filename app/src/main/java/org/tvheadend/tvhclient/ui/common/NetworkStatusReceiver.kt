afd12e10791e2091fd503261016724ecb399fabf
Robert Siebert <rsiebert80@gmail.com>
2019-11-21 14:07:52 +0100

    Moved some methods into the network receiver class

--- app/src/main/java/org/tvheadend/tvhclient/ui/common/NetworkStatusReceiver.kt
+++ app/src/main/java/org/tvheadend/tvhclient/ui/common/NetworkStatusReceiver.kt
@@ -3,15 +3,89 @@ package org.tvheadend.tvhclient.ui.common
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.wifi.WifiManager
+import android.os.Build
 import org.tvheadend.tvhclient.repository.AppRepository
 import timber.log.Timber
+import java.lang.reflect.InvocationTargetException
+
+enum class NetworkStatus {
+    NETWORK_UNKNOWN,
+    NETWORK_IS_DOWN,
+    NETWORK_IS_UP,
+    NETWORK_IS_STILL_DOWN,
+    NETWORK_IS_STILL_UP
+}
 
 class NetworkStatusReceiver(private val appRepository: AppRepository) : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        val isAvailable = isConnectionAvailable(context)
+        val isAvailable = isNetworkAvailable(context) || isWifiApEnabled(context)
         Timber.d("Network availability is $isAvailable")
         val networkIsAvailable = getNetworkStatus(appRepository.getNetworkStatus().value, isAvailable)
         appRepository.setNetworkStatus(networkIsAvailable)
+    }
+
+    @Suppress("DEPRECATION")
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            (activeNetworkInfo != null && activeNetworkInfo.isConnected)
+        } else {
+            val activeNetwork = connectivityManager.activeNetwork
+            val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+            (networkCapabilities != null
+                    && (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+                    || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH)
+                    || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+                    || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)
+                    || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)))
+        }
+    }
+
+    private fun isWifiApEnabled(context: Context): Boolean {
+        val wifiManager = context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val wmMethods = wifiManager.javaClass.declaredMethods
+        for (method in wmMethods) {
+            if (method.name == "isWifiApEnabled") {
+                try {
+                    return method.invoke(wifiManager) as Boolean
+                } catch (e: IllegalArgumentException) {
+                    Timber.d(e, "Illegal argument error while invoking method 'isWifiApEnabled'")
+                } catch (e: IllegalAccessException) {
+                    Timber.d(e, "Illegal access error while invoking method 'isWifiApEnabled'")
+                } catch (e: InvocationTargetException) {
+                    Timber.d(e, "Invocation error while invoking method 'isWifiApEnabled'")
+                }
+            }
+        }
+        return false
+    }
+
+    private fun getNetworkStatus(previousStatus: NetworkStatus?, isAvailable: Boolean): NetworkStatus {
+        Timber.d("Previous network status is $previousStatus")
+        val status = when (previousStatus) {
+            NetworkStatus.NETWORK_IS_DOWN -> {
+                if (isAvailable) NetworkStatus.NETWORK_IS_UP else NetworkStatus.NETWORK_IS_STILL_DOWN
+            }
+            NetworkStatus.NETWORK_IS_UP -> {
+                if (isAvailable) NetworkStatus.NETWORK_IS_STILL_UP else NetworkStatus.NETWORK_IS_DOWN
+            }
+            NetworkStatus.NETWORK_IS_STILL_DOWN -> {
+                if (isAvailable) NetworkStatus.NETWORK_IS_UP else NetworkStatus.NETWORK_IS_STILL_DOWN
+            }
+            NetworkStatus.NETWORK_IS_STILL_UP -> {
+                if (isAvailable) NetworkStatus.NETWORK_IS_STILL_UP else NetworkStatus.NETWORK_IS_DOWN
+            }
+            NetworkStatus.NETWORK_UNKNOWN -> {
+                if (isAvailable) NetworkStatus.NETWORK_IS_UP else NetworkStatus.NETWORK_IS_DOWN
+            }
+            else -> return NetworkStatus.NETWORK_UNKNOWN
+        }
+        Timber.d("Current network status is $status")
+        return status
     }
 }
