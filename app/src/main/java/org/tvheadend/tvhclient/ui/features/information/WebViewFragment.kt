1a910f14bcaea1eceb3cb2cdecd674e516f3790d
Robert Siebert <rsiebert80@gmail.com>
2020-02-26 14:46:14 +0100

    Moved the loading of files into a separate class Show an error message when no content could be loaded Get the views in the old fashioned way to prepare for view binding

--- app/src/main/java/org/tvheadend/tvhclient/ui/features/information/WebViewFragment.kt
+++ app/src/main/java/org/tvheadend/tvhclient/ui/features/information/WebViewFragment.kt
@@ -1,105 +1,60 @@
 package org.tvheadend.tvhclient.ui.features.information
 
-import android.content.Context
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.*
+import android.webkit.WebView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.preference.PreferenceManager
-import kotlinx.android.synthetic.main.webview_fragment.*
-import kotlinx.coroutines.*
-import org.tvheadend.tvhclient.BuildConfig
 import org.tvheadend.tvhclient.R
 import org.tvheadend.tvhclient.ui.base.LayoutControlInterface
-import org.tvheadend.tvhclient.ui.common.getLocale
-import org.tvheadend.tvhclient.ui.common.interfaces.ToolbarInterface
+import org.tvheadend.tvhclient.ui.common.interfaces.FileContentsLoadedInterface
 import org.tvheadend.tvhclient.ui.features.settings.RemoveFragmentFromBackstackInterface
 import org.tvheadend.tvhclient.util.extensions.gone
 import org.tvheadend.tvhclient.util.extensions.visible
-import org.tvheadend.tvhclient.util.getThemeId
+import org.tvheadend.tvhclient.util.extensions.visibleOrGone
 import timber.log.Timber
-import java.io.InputStream
-import java.util.regex.Pattern
 
-open class WebViewFragment : Fragment() {
+open class WebViewFragment : Fragment(), FileContentsLoadedInterface {
 
-    private val job = Job()
-    private val scope = CoroutineScope(Dispatchers.IO + job)
-
-    protected lateinit var toolbarInterface: ToolbarInterface
     var website: String = ""
+    private var webView: WebView? = null
+    private var loadingView: ProgressBar? = null
+    private var errorTextView: TextView? = null
+    private lateinit var fileContentLoader: FileContentLoader
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return if (Build.VERSION.SDK_INT in 21..25) {
-            inflater.inflate(R.layout.webview_fragment_for_lollipop, container, false)
-        } else {
-            inflater.inflate(R.layout.webview_fragment, container, false)
-        }
+        val view = inflater.inflate(if (Build.VERSION.SDK_INT in 21..25) R.layout.webview_fragment_for_lollipop else R.layout.webview_fragment, container, false)
+        webView = view.findViewById(R.id.webview)
+        loadingView = view.findViewById(R.id.loading_view)
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
         if (activity is LayoutControlInterface) {
             (activity as LayoutControlInterface).forceSingleScreenLayout()
         }
-        if (activity is ToolbarInterface) {
-            toolbarInterface = activity as ToolbarInterface
-        }
+
+        fileContentLoader = FileContentLoader(context!!, "en", this)
+        // Make the background transparent to remove flickering. This avoids seeing
+        // the default theme background color before the stylesheets are loaded.
+        webView?.setBackgroundColor(Color.argb(0, 0, 0, 0))
+        webView?.gone()
 
         setHasOptionsMenu(true)
-
-        // Make the background transparent to remove flickering.
-        // This avoids seeing the default theme background color
-        // before the stylesheets are loaded.
-        webview.setBackgroundColor(Color.argb(0, 0, 0, 0))
-        webview.gone()
-    }
-
-    /**
-     * Reads the contents of the HTML file of the defined language.
-     * The language is determined by the locale. If the file could
-     * not be loaded, then a default file (English) will be loaded.
-     */
-    private suspend fun loadFileContents(context: Context, filename: String, defaultLocale: String) {
-        val deferredLoader = scope.async {
-            val languageCode = PreferenceManager.getDefaultSharedPreferences(context).getString("language", getLocale(context).language)!!.substring(0, 2)
-            val htmlFile = "html/" + filename + "_" + languageCode.substring(0, 2) + ".html"
-            val defaultHtmlFile = "html/" + filename + "_" + defaultLocale + ".html"
-
-            var contents = loadContentsFromAssetFile(context, htmlFile)
-            if (contents.isEmpty()) {
-                contents = loadContentsFromAssetFile(context, defaultHtmlFile)
-            }
-            return@async contents
-        }
-        // Switch the context to the main thread to call the following method
-        withContext(Dispatchers.Main) {
-            onFileContentsLoaded((deferredLoader.await()))
-        }
-    }
-
-    private fun loadContentsFromAssetFile(context: Context, filename: String): String {
-        var contents = ""
-        try {
-            Timber.d("Reading contents of file $filename")
-            val inputStream: InputStream = context.assets.open(filename)
-            contents = inputStream.bufferedReader().use { it.readText() }
-        } catch (e: Exception) {
-            Timber.d("Could not open or read contents of file $filename")
-        }
-        return contents
     }
 
     override fun onResume() {
         super.onResume()
-        scope.launch { loadFileContents(context!!, website, "en") }
+        fileContentLoader.start(website)
     }
 
     override fun onPause() {
         super.onPause()
-        job.cancel()
+        fileContentLoader.stop()
     }
 
     override fun onPrepareOptionsMenu(menu: Menu) {
@@ -114,10 +69,8 @@ open class WebViewFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
-                activity.let {
-                    if (it is RemoveFragmentFromBackstackInterface) {
-                        it.removeFragmentFromBackstack()
-                    }
+                if (activity is RemoveFragmentFromBackstackInterface) {
+                    (activity as RemoveFragmentFromBackstackInterface).removeFragmentFromBackstack()
                 }
                 true
             }
@@ -125,27 +78,14 @@ open class WebViewFragment : Fragment() {
         }
     }
 
-    private fun onFileContentsLoaded(fileContent: String) {
+    override fun onFileContentsLoaded(content: String) {
         Timber.d("File contents loaded")
-        val ctx = context ?: return
-        var content = fileContent
-        if (content.isNotEmpty()) {
-            if (content.contains("styles_light.css")) {
-                content = if (getThemeId(ctx) == R.style.CustomTheme_Light) {
-                    content.replace("styles_light.css", "html/styles_light.css")
-                } else {
-                    content.replace("styles_light.css", "html/styles_dark.css")
-                }
-            }
-            if (content.contains("APP_VERSION")) {
-                // Replace the placeholder in the html file with the real version
-                val version = BuildConfig.VERSION_NAME + " (" + BuildConfig.BUILD_VERSION + ")"
-                content = Pattern.compile("APP_VERSION").matcher(content).replaceAll(version)
-            }
+        loadingView?.gone()
+        errorTextView?.visibleOrGone(content.isEmpty())
 
-            webview.loadDataWithBaseURL("file:///android_asset/", content, "text/html", "utf-8", null)
-            loading_view.gone()
-            webview.visible()
+        if (content.isNotEmpty()) {
+            webView?.loadDataWithBaseURL("file:///android_asset/", content, "text/html", "utf-8", null)
+            webView?.visible()
         }
     }
 }
