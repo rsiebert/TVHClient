7f5230ea9c710dee5d86fe2bb7f5f1e6b2f4b9db
Robert Siebert <rsiebert80@gmail.com>
2020-04-22 20:42:38 +0200

    Remove unused method

--- app/src/main/java/org/tvheadend/tvhclient/ui/features/startup/StartupViewModel.kt
+++ app/src/main/java/org/tvheadend/tvhclient/ui/features/startup/StartupViewModel.kt
@@ -2,15 +2,12 @@ package org.tvheadend.tvhclient.ui.features.startup
 
 import android.app.Application
 import android.content.Context
-import android.content.Intent
 import android.content.SharedPreferences
 import androidx.core.util.Pair
 import androidx.lifecycle.*
 import org.tvheadend.data.AppRepository
 import org.tvheadend.data.entity.Connection
 import org.tvheadend.tvhclient.MainApplication
-import org.tvheadend.tvhclient.service.HtspService
-import timber.log.Timber
 import javax.inject.Inject
 
 open class StartupViewModel(application: Application) : AndroidViewModel(application) {
@@ -52,18 +49,5 @@ open class StartupViewModel(application: Application) : AndroidViewModel(applica
 
     private fun inject() {
         MainApplication.component.inject(this)
-    }
-
-    fun updateConnectionAndRestartApplication(context: Context?, isSyncRequired: Boolean = true) {
-        Timber.d("Restart of application requested")
-        context?.let {
-            if (isSyncRequired) {
-                appRepository.connectionData.setSyncRequiredForActiveConnection()
-            }
-            context.stopService(Intent(context, HtspService::class.java))
-            val intent = Intent(context, SplashActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            context.startActivity(intent)
-        }
     }
 }
