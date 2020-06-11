591bfd535a0820275f8ef3bcea1b08c8b6101ab0
Robert Siebert <rsiebert80@gmail.com>
2020-06-11 12:26:57 +0200

    Added remaining conversion to and from an entity

--- data/src/main/java/org/tvheadend/data/source/SeriesRecordingDataSource.kt
+++ data/src/main/java/org/tvheadend/data/source/SeriesRecordingDataSource.kt
@@ -1,12 +1,14 @@
 package org.tvheadend.data.source
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.tvheadend.data.db.AppRoomDatabase
 import org.tvheadend.data.entity.SeriesRecording
+import org.tvheadend.data.entity.SeriesRecordingEntity
 import java.util.*
 
 class SeriesRecordingDataSource(private val db: AppRoomDatabase) : DataSourceInterface<SeriesRecording> {
@@ -14,15 +16,15 @@ class SeriesRecordingDataSource(private val db: AppRoomDatabase) : DataSourceInt
     private val ioScope = CoroutineScope(Dispatchers.IO)
 
     override fun addItem(item: SeriesRecording) {
-        ioScope.launch { db.seriesRecordingDao.insert(item) }
+        ioScope.launch { db.seriesRecordingDao.insert(SeriesRecordingEntity.from(item)) }
     }
 
     override fun updateItem(item: SeriesRecording) {
-        ioScope.launch { db.seriesRecordingDao.update(item) }
+        ioScope.launch { db.seriesRecordingDao.update(SeriesRecordingEntity.from(item)) }
     }
 
     override fun removeItem(item: SeriesRecording) {
-        ioScope.launch { db.seriesRecordingDao.delete(item) }
+        ioScope.launch { db.seriesRecordingDao.delete(SeriesRecordingEntity.from(item)) }
     }
 
     override fun getLiveDataItemCount(): LiveData<Int> {
@@ -30,18 +32,22 @@ class SeriesRecordingDataSource(private val db: AppRoomDatabase) : DataSourceInt
     }
 
     override fun getLiveDataItems(): LiveData<List<SeriesRecording>> {
-        return db.seriesRecordingDao.loadAllRecordings()
+        return Transformations.map(db.seriesRecordingDao.loadAllRecordings()) { entities ->
+            entities.map { it.toRecording() }
+        }
     }
 
     override fun getLiveDataItemById(id: Any): LiveData<SeriesRecording> {
-        return db.seriesRecordingDao.loadRecordingById(id as String)
+        return Transformations.map(db.seriesRecordingDao.loadRecordingById(id as String)) { entity ->
+            entity.toRecording()
+        }
     }
 
     override fun getItemById(id: Any): SeriesRecording? {
         var seriesRecording: SeriesRecording? = null
         if ((id as String).isNotEmpty()) {
             runBlocking(Dispatchers.IO) {
-                seriesRecording = db.seriesRecordingDao.loadRecordingByIdSync(id)
+                seriesRecording = db.seriesRecordingDao.loadRecordingByIdSync(id)?.toRecording()
             }
         }
         return seriesRecording
