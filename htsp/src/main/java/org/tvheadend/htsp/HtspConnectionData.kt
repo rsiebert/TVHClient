fa19e245a264912d8b241c19cc0a1e902a118dce
Robert Siebert <rsiebert80@gmail.com>
2020-10-22 14:15:53 +0200

    Added sealed classes to replace enums later

--- htsp/src/main/java/org/tvheadend/htsp/HtspConnectionData.kt
+++ htsp/src/main/java/org/tvheadend/htsp/HtspConnectionData.kt
@@ -8,3 +8,20 @@ data class HtspConnectionData(
         var versionCode: Int = 0,
         var connectionTimeout: Int = 0,
 )
+
+sealed class HtspAuthenticationStateResult {
+    data class Idle(val message: String) : HtspAuthenticationStateResult()
+    data class InProgress(val message: String) : HtspAuthenticationStateResult()
+    data class Authenticated(val message: String) : HtspAuthenticationStateResult()
+    // Bad credentials or any other failure
+    data class Failed(val message: String) : HtspAuthenticationStateResult()
+}
+
+sealed class HtspConnectionStateResult {
+    data class Idle(val message: String) : HtspConnectionStateResult()
+    data class Closed(val message: String) : HtspConnectionStateResult()
+    data class Connecting(val message: String) : HtspConnectionStateResult()
+    data class Connected(val message: String) : HtspConnectionStateResult()
+    // Failed, interrupted, unresolved address, error connecting to server or exception while opening socket
+    data class Failed(val message: String) : HtspConnectionStateResult()
+}
