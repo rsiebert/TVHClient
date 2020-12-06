244f3ad6c809bcbaa346dd9b0a9104f2a0a7fce4
Robert Siebert <rsiebert80@gmail.com>
2020-12-06 16:24:51 +0100

    Map only non null items into the list

--- data/src/main/java/org/tvheadend/data/source/ProgramDataSource.kt
+++ data/src/main/java/org/tvheadend/data/source/ProgramDataSource.kt
@@ -30,7 +30,7 @@ class ProgramDataSource(private val db: AppRoomDatabase) : DataSourceInterface<P
 
     fun addItems(items: List<Program>) {
         if (!items.isNullOrEmpty()) {
-            ioScope.launch { db.programDao.insert(ArrayList(items).map { ProgramEntity.from(it) }) }
+            ioScope.launch { db.programDao.insert(ArrayList(items).mapNotNull { ProgramEntity.from(it) }) }
         }
     }
 
