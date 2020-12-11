38cf3eba8b49749939a9e3c9aeac0793401126f6
Robert Siebert <rsiebert80@gmail.com>
2020-12-11 14:38:47 +0100

    The http service handlers shall use the same interfaces as the htsp service handlers

--- app/src/main/java/org/tvheadend/tvhclient/service/http/HttpIntentServiceHandler.kt
+++ app/src/main/java/org/tvheadend/tvhclient/service/http/HttpIntentServiceHandler.kt
@@ -3,11 +3,14 @@ package org.tvheadend.tvhclient.service.http
 import android.content.Context
 import android.content.Intent
 import androidx.preference.PreferenceManager
+import org.tvheadend.api.AuthenticationStateResult
+import org.tvheadend.api.ConnectionStateResult
+import org.tvheadend.api.ServerConnectionStateListener
 import org.tvheadend.data.AppRepository
 import org.tvheadend.data.entity.Connection
 import org.tvheadend.tvhclient.service.ConnectionIntentService
 
-class HttpIntentServiceHandler(val context: Context, val appRepository: AppRepository, val connection: Connection) : ConnectionIntentService.ServiceInterface {
+class HttpIntentServiceHandler(val context: Context, val appRepository: AppRepository, val connection: Connection) : ConnectionIntentService.ServiceInterface, ServerConnectionStateListener {
 
     private val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
 
@@ -16,6 +19,14 @@ class HttpIntentServiceHandler(val context: Context, val appRepository: AppRepos
     }
 
     override fun onDestroy() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAuthenticationStateChange(result: AuthenticationStateResult) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onConnectionStateChange(result: ConnectionStateResult) {
         TODO("Not yet implemented")
     }
 }
