8349d3104a06d89493d0adc29b85d06fe1c63f9e
Robert Siebert <rsiebert80@gmail.com>
2021-03-26 15:53:23 +0100

    Replaced async task

--- app/src/main/java/org/tvheadend/tvhclient/ui/features/playback/external/ConvertHostnameToAddressTask.kt
+++ app/src/main/java/org/tvheadend/tvhclient/ui/features/playback/external/ConvertHostnameToAddressTask.kt
@@ -1,18 +1,28 @@
 package org.tvheadend.tvhclient.ui.features.playback.external
 
-import android.os.AsyncTask
+import kotlinx.coroutines.CoroutineScope
+import org.tvheadend.tvhclient.util.extensions.executeAsyncTask
 import timber.log.Timber
 import java.net.InetAddress
 import java.net.UnknownHostException
 
-internal class ConvertHostnameToAddressTask(private val hostname: String) : AsyncTask<Void, Void, String>() {
+class ConvertHostnameToAddressTask(lifecycleScope: CoroutineScope, private val hostname: String) {
 
-    override fun doInBackground(vararg voids: Void): String {
-        return try {
-            InetAddress.getByName(hostname).hostAddress
-        } catch (e: UnknownHostException) {
-            Timber.d(e, "Could not get ip address from $hostname, using hostname as fallback")
-            hostname
-        }
+    init {
+        lifecycleScope.executeAsyncTask(onPreExecute = {
+            // ... runs in Main Thread
+            Timber.d("onPreExecute")
+        }, doInBackground = {
+            Timber.d("doInBackground")
+            try {
+                InetAddress.getByName(hostname).hostAddress
+            } catch (e: UnknownHostException) {
+                Timber.d(e, "Could not get ip address from $hostname, using hostname as fallback")
+                hostname
+            }
+        }, onPostExecute = {
+            // runs in Main Thread
+            Timber.d("onPostExecute")
+        })
     }
 }
