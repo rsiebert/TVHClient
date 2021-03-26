1dff4b7d475d78ad7fc1ab68a6486e7231ec9acb
Robert Siebert <rsiebert80@gmail.com>
2021-03-26 15:52:40 +0100

    Use the java version of uri

--- app/src/main/java/org/tvheadend/tvhclient/ui/features/settings/ConnectionValidator.kt
+++ app/src/main/java/org/tvheadend/tvhclient/ui/features/settings/ConnectionValidator.kt
@@ -1,8 +1,9 @@
 package org.tvheadend.tvhclient.ui.features.settings
 
-import android.net.Uri
 import org.tvheadend.data.entity.Connection
 import timber.log.Timber
+import java.net.URI
+import java.net.URISyntaxException
 import java.util.regex.Pattern
 
 class ConnectionValidator {
@@ -54,7 +55,12 @@ class ConnectionValidator {
         if (value.isNullOrEmpty()) {
             return ValidationResult.Failed(ValidationFailureReason.UrlEmpty())
         }
-        val uri = Uri.parse(value)
+        lateinit var uri: URI
+        try {
+            uri = URI(value)
+        } catch (e: URISyntaxException) {
+            return ValidationResult.Failed(ValidationFailureReason.UrlSchemeMissing())
+        }
         if (uri.scheme.isNullOrEmpty()) {
             return ValidationResult.Failed(ValidationFailureReason.UrlSchemeMissing())
         }
@@ -100,20 +106,20 @@ class ConnectionValidator {
 }
 
 sealed class ValidationResult {
-    data class Success(val message: String = ""): ValidationResult()
-    data class Failed(val reason: ValidationFailureReason): ValidationResult()
+    data class Success(val message: String = "") : ValidationResult()
+    data class Failed(val reason: ValidationFailureReason) : ValidationResult()
 }
 
 sealed class ValidationFailureReason {
-    data class NameEmpty(val message: String = ""): ValidationFailureReason()
-    data class NameInvalid(val message: String = ""): ValidationFailureReason()
-    data class UrlEmpty(val message: String = ""): ValidationFailureReason()
-    data class UrlSchemeMissing(val message: String = ""): ValidationFailureReason()
-    data class UrlSchemeWrong(val message: String = ""): ValidationFailureReason()
-    data class UrlHostMissing(val message: String = ""): ValidationFailureReason()
-    data class UrlPortMissing(val message: String = ""): ValidationFailureReason()
-    data class UrlPortInvalid(val message: String = ""): ValidationFailureReason()
-    data class UrlUnneededCredentials(val message: String = ""): ValidationFailureReason()
-    data class MacAddressEmpty(val message: String = ""): ValidationFailureReason()
-    data class MacAddressInvalid(val message: String = ""): ValidationFailureReason()
+    data class NameEmpty(val message: String = "") : ValidationFailureReason()
+    data class NameInvalid(val message: String = "") : ValidationFailureReason()
+    data class UrlEmpty(val message: String = "") : ValidationFailureReason()
+    data class UrlSchemeMissing(val message: String = "") : ValidationFailureReason()
+    data class UrlSchemeWrong(val message: String = "") : ValidationFailureReason()
+    data class UrlHostMissing(val message: String = "") : ValidationFailureReason()
+    data class UrlPortMissing(val message: String = "") : ValidationFailureReason()
+    data class UrlPortInvalid(val message: String = "") : ValidationFailureReason()
+    data class UrlUnneededCredentials(val message: String = "") : ValidationFailureReason()
+    data class MacAddressEmpty(val message: String = "") : ValidationFailureReason()
+    data class MacAddressInvalid(val message: String = "") : ValidationFailureReason()
 }
